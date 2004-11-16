88d9df0bc31103cbb8b2bb54c7d1cf6d4c34fd86
Joachim Falk <falk@cs.fau.de>
2004-11-16 15:29:31 +0000

    Forgot to add hscd_fifo.cpp and hscd_rendezvous.cpp
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SysteMoC--mainline--0.1--patch-10

---
+++ hscd_fifo.cpp
@@ -0,0 +1,5 @@
+// vim: set sw=2 ts=8:
+
+#include <hscd_fifo.hpp>
+
+const char* const hscd_fifo::chan_kind::kind_string = "hscd_fifo";
---
+++ hscd_rendezvous.cpp
@@ -0,0 +1,5 @@
+// vim: set sw=2 ts=8:
+
+#include <hscd_rendezvous.hpp>
+
+const char* const hscd_rendezvous::chan_kind::kind_string = "hscd_rendezvous";
