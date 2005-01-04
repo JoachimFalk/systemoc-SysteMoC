eab2ec82c5b2aa77fed7f4204118bfa6ea4f4697
Joachim Falk <joachim.falk@gmx.de>
2005-01-04 09:29:46 +0000

    New SystemC-Eamples V0.2 for SysteMoC V0.2
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SystemC-Examples--mainline--0.2--patch-1

--- systemc-csp/design-csp.cpp
+++ systemc-csp/design-csp.cpp
@@ -8,108 +8,114 @@
 #include <hscd_rendezvous.hpp>
 #include <hscd_node_types.hpp>
 #ifndef __SCFE__
-# include <hscd_scheduler.hpp>
+//# include <hscd_scheduler.hpp>
 # include <hscd_pggen.hpp>
 #endif
 
 enum dp_forkreq_ty { FORK_TAKE, FORK_DROP };
 
 class dp_fork
-  : public hscd_choice_active_node {
+  : public hscd_choice_passive_node {
 public:
   hscd_port_in<dp_forkreq_ty> l_forkreq;
   hscd_port_in<dp_forkreq_ty> r_forkreq;
 private:
-  void process() {
-    while ( 1 ) {
-      choice( l_forkreq(1) | r_forkreq(1) );
-      assert( (l_forkreq && !r_forkreq && l_forkreq[0] == FORK_TAKE) ||
-              (r_forkreq && !l_forkreq && r_forkreq[0] == FORK_TAKE) );
-      if ( l_forkreq ) {
-        std::cout << "Fork " << name() << " taken by left philosopher !" << std::endl;
-        transact( l_forkreq(1) );
-        assert( l_forkreq && l_forkreq[0] == FORK_DROP );
-        std::cout << "Fork " << name() << " droped by left philosopher !" << std::endl;
-      } else {
-        std::cout << "Fork " << name() << " taken by right philosopher !" << std::endl;
-        transact( r_forkreq(1) );
-        assert( r_forkreq && r_forkreq[0] == FORK_DROP );
-        std::cout << "Fork " << name() << " droped by right philosopher !" << std::endl;
-      }
-    }
+  void l_forkreq_take() { assert(l_forkreq[0] == FORK_TAKE);
+    std::cout << "Fork " << name() << " taken by left philosopher !" << std::endl; }
+  void l_forkreq_drop() { assert(l_forkreq[0] == FORK_DROP);
+    std::cout << "Fork " << name() << " droped by left philosopher !" << std::endl; }
+  void r_forkreq_take() { assert(r_forkreq[0] == FORK_TAKE);
+    std::cout << "Fork " << name() << " taken by right philosopher !" << std::endl; }
+  void r_forkreq_drop() { assert(r_forkreq[0] == FORK_DROP);
+    std::cout << "Fork " << name() << " droped by right philosopher !" << std::endl; }
+  
+  hscd_firing_state fireRules() {
+    hscd_firing_state start =
+      Choice( l_forkreq(1) >> call(&dp_fork::l_forkreq_take,
+                Choice( l_forkreq(1) >> call(&dp_fork::l_forkreq_drop, &start) ) ) |
+              r_forkreq(1) >> call(&dp_fork::r_forkreq_take,
+                Choice( r_forkreq(1) >> call(&dp_fork::r_forkreq_drop, &start) ) )
+            );
+    return start;
   }
 public:
   dp_fork( sc_module_name name )
-    : hscd_choice_active_node(name) {}
+    : hscd_choice_passive_node(name, fireRules() ) {}
 };
 
 class dp_footman
-  : public hscd_choice_active_node {
+  : public hscd_choice_passive_node {
 public:
+  hscd_port_in<void> sitreq_0;
   hscd_port_in<void> sitreq_1;
-  hscd_port_in<void> sitreq_2;
-  hscd_port_in<void> sitreq_3;
-  hscd_port_in<void> sitreq_4;
+  hscd_port_in<void> standreq_0;
   hscd_port_in<void> standreq_1;
-  hscd_port_in<void> standreq_2;
-  hscd_port_in<void> standreq_3;
-  hscd_port_in<void> standreq_4;
 private:
-  void process() {
-    int count = 4;
-    
-    while ( 1 ) {
-      if ( count != 0 )
-        choice( sitreq_1(1) | standreq_1(1) |
-                sitreq_2(1) | standreq_2(1) |
-                sitreq_3(1) | standreq_3(1) |
-                sitreq_4(1) | standreq_4(1) );
-      else
-        choice(               standreq_1(1) |
-                              standreq_2(1) |
-                              standreq_3(1) |
-                              standreq_4(1) );
-      if ( sitreq_1 || sitreq_2 || sitreq_3 || sitreq_4 )
-        --count;
-      else
-        ++count;
-    }
+  void philosopher0_sitdown() {
+    std::cout << "Seat " << name() << " taken by philosopher 0 !" << std::endl; }
+  void philosopher0_standup() {
+    std::cout << "Seat " << name() << " vacated by philosopher 0 !" << std::endl; }
+  void philosopher1_sitdown() {
+    std::cout << "Seat " << name() << " taken by philosopher 1 !" << std::endl; }
+  void philosopher1_standup() {
+    std::cout << "Seat " << name() << " vacated by philosopher 1 !" << std::endl; }
+  
+  hscd_firing_state fireRules() {
+    hscd_firing_state start =
+      Choice( sitreq_0(1) >> call(&dp_footman::philosopher0_sitdown,
+                Choice( standreq_0(1) >> call(&dp_footman::philosopher0_standup, &start ) ) ) |
+              sitreq_1(1) >> call(&dp_footman::philosopher1_sitdown,
+                Choice( standreq_1(1) >> call(&dp_footman::philosopher1_standup, &start ) ) )
+            );
+    return start;
   }
 public:
   dp_footman( sc_module_name name )
-    : hscd_choice_active_node(name) {}
+    : hscd_choice_passive_node(name, fireRules() ) {}
 };
 
 class dp_philosopher
-  : public hscd_choice_active_node {
+  : public hscd_choice_passive_node {
 public:
   hscd_port_out<dp_forkreq_ty> l_forkreq;
   hscd_port_out<dp_forkreq_ty> r_forkreq;
   hscd_port_out<void>          sitreq;
   hscd_port_out<void>          standreq;
 private:
-  void process() {
-    while ( 1 ) {
-      std::cout << "Philosopher " << name() << " want's to eat !" << std::endl;
-      transact( sitreq(1) );
-      l_forkreq[0] = FORK_TAKE;
-      transact( l_forkreq(1) );
-      r_forkreq[0] = FORK_TAKE;
-      transact( r_forkreq(1) );
-      std::cout << "Philosopher " << name() << " eating !" << std::endl;
-      wait( sc_time(3,SC_NS) );
-      l_forkreq[0] = FORK_DROP;
-      transact( l_forkreq(1) );
-      r_forkreq[0] = FORK_DROP;
-      transact( r_forkreq(1) );
-      std::cout << "Philosopher " << name() << " finish eating !" << std::endl;
-      transact( standreq(1) );
-      wait( sc_time(1,SC_NS) );
-    }
+  void nothing() {}
+  
+  void eat() {
+    std::cout << "Philosopher " << name() << " eating !" << std::endl;
+    wait( sc_time(3,SC_NS) );
+    l_forkreq[0] = r_forkreq[0] = FORK_DROP;
+  }
+  
+  void think() {
+    std::cout << "Philosopher " << name() << " thinking !" << std::endl;
+    wait( sc_time(100,SC_NS) );
+    l_forkreq[0] = r_forkreq[0] = FORK_TAKE; 
+  }
+  
+  hscd_firing_state fireRules() {
+    hscd_firing_state start =
+      Choice( sitreq(1) >> call(&dp_philosopher::nothing,
+        Choice( l_forkreq(1) >> call(&dp_philosopher::nothing,
+          Choice( r_forkreq(1) >> call(&dp_philosopher::eat,
+            Choice( r_forkreq(1) >> call(&dp_philosopher::nothing,
+              Choice( l_forkreq(1) >> call(&dp_philosopher::nothing,
+                Choice( standreq(1) >> call(&dp_philosopher::think, &start ) )
+              ) )
+            ) )
+          ) )
+        ) )
+      ) );
+    return start;
   }
 public:
   dp_philosopher( sc_module_name name )
-    : hscd_choice_active_node(name) {}
+    : hscd_choice_passive_node(name, fireRules() ) {
+    l_forkreq[0] = r_forkreq[0] = FORK_TAKE; 
+  }
 };
 
 class m_top
@@ -117,39 +123,30 @@ class m_top
   public:
     m_top( sc_module_name name )
       : hscd_csp_constraintset(name) {
+      std::cout << "Instantiating " << name << " !" << std::endl;
+      std::cout << "Instantiating fork0 !" << std::endl;
+      dp_fork        &m_fork0        = registerNode(new dp_fork("m_fork0"));
+      std::cout << "Instantiating fork1 !" << std::endl;
       dp_fork        &m_fork1        = registerNode(new dp_fork("m_fork1"));
-      dp_fork        &m_fork2        = registerNode(new dp_fork("m_fork2"));
-      dp_fork        &m_fork3        = registerNode(new dp_fork("m_fork3"));
-      dp_fork        &m_fork4        = registerNode(new dp_fork("m_fork4"));
+      std::cout << "Instantiating philosopher0 !" << std::endl;
+      dp_philosopher &m_philosopher0 = registerNode(new dp_philosopher("m_philosopher0"));
+      std::cout << "Instantiating philosopher1 !" << std::endl;
       dp_philosopher &m_philosopher1 = registerNode(new dp_philosopher("m_philosopher1"));
-      dp_philosopher &m_philosopher2 = registerNode(new dp_philosopher("m_philosopher2"));
-      dp_philosopher &m_philosopher3 = registerNode(new dp_philosopher("m_philosopher3"));
-      dp_philosopher &m_philosopher4 = registerNode(new dp_philosopher("m_philosopher4"));
+      std::cout << "Instantiating footman !" << std::endl;
       dp_footman     &m_footman      = registerNode(new dp_footman("m_footman"));
       
+      connectNodePorts( m_philosopher0.l_forkreq, m_fork0.r_forkreq );
+      connectNodePorts( m_philosopher1.r_forkreq, m_fork0.l_forkreq );
+      
       connectNodePorts( m_philosopher1.l_forkreq, m_fork1.r_forkreq );
-      connectNodePorts( m_philosopher4.r_forkreq, m_fork1.l_forkreq );
+      connectNodePorts( m_philosopher0.r_forkreq, m_fork1.l_forkreq );
       
-      connectNodePorts( m_philosopher2.l_forkreq, m_fork2.r_forkreq );
-      connectNodePorts( m_philosopher1.r_forkreq, m_fork2.l_forkreq );
-      
-      connectNodePorts( m_philosopher3.l_forkreq, m_fork3.r_forkreq );
-      connectNodePorts( m_philosopher2.r_forkreq, m_fork3.l_forkreq );
-      
-      connectNodePorts( m_philosopher4.l_forkreq, m_fork4.r_forkreq );
-      connectNodePorts( m_philosopher3.r_forkreq, m_fork4.l_forkreq );
-      
+      connectNodePorts( m_philosopher0.sitreq,   m_footman.sitreq_0   );
+      connectNodePorts( m_philosopher0.standreq, m_footman.standreq_0 );
+
       connectNodePorts( m_philosopher1.sitreq,   m_footman.sitreq_1   );
       connectNodePorts( m_philosopher1.standreq, m_footman.standreq_1 );
-
-      connectNodePorts( m_philosopher2.sitreq,   m_footman.sitreq_2   );
-      connectNodePorts( m_philosopher2.standreq, m_footman.standreq_2 );
-
-      connectNodePorts( m_philosopher3.sitreq,   m_footman.sitreq_3   );
-      connectNodePorts( m_philosopher3.standreq, m_footman.standreq_3 );
-
-      connectNodePorts( m_philosopher4.sitreq,   m_footman.sitreq_4   );
-      connectNodePorts( m_philosopher4.standreq, m_footman.standreq_4 );
+      std::cout << "Instantiating " << name << " finished !" << std::endl;
     }
 };
 
@@ -159,8 +156,9 @@ int sc_main (int argc, char **argv) {
 #endif
     hscd_csp_moc<m_top> *top = new hscd_csp_moc<m_top>("top");
 #ifndef __SCFE__
-    hscd_top x(top);
+//    hscd_top x(top);
     
+    std::cout << "Was here !" << std::endl;
     hscd_modes::dump( std::cout, *top );
     
     sc_start(-1);
--- systemc-df2/design-df.cpp
+++ systemc-df2/design-df.cpp
@@ -1,22 +1,33 @@
 // vim: set sw=2 ts=8:
 
-#include <hscd_port.hpp>
-#include <hscd_fifo.hpp>
-#include <hscd_node_types.hpp>
-#include <hscd_structure.hpp>
-#include <hscd_scheduler.hpp>
 #include <cstdlib>
 #include <iostream>
 
+#include <hscd_moc.hpp>
+#include <hscd_port.hpp>
+#include <hscd_fifo.hpp>
+#include <hscd_node_types.hpp>
+#ifndef __SCFE__
+//# include <hscd_scheduler.hpp>
+# include <hscd_pggen.hpp>
+#endif
+
+#define DEBUG 1
 
 // queue pair number
 typedef int t_qpn;
 // transmit token
 typedef int t_TT;
 
+struct markers {
+  hscd_firing_state start;
+  hscd_firing_state other;
+};
+
 
 class m_h_sched :
-	public hscd_transact_active_node {
+        public markers,
+	public hscd_choice_passive_node {
 public:
 	hscd_port_out<t_qpn> out_getTT;
 	hscd_port_in<t_TT> in_getTT;
@@ -24,97 +35,78 @@ public:
         hscd_port_out<void> out_void;
 
 private:
-	void process () {
-		t_qpn qpn;
-		t_TT TT;
-
-		qpn = 42;
-
-		//out_getTT.write(qpn);
-		out_getTT[0] = qpn;
-		transact(out_getTT(1));
-		
-		cout << " S> t out\n";
-
-		transact(out_getTT(1) & in_getTT(1));
-		cout << " S> t in\n";
-
-		//TT = in_getTT.read();
-		TT = in_getTT[0];
-
-		cout << "SCHED> just got TT for qpn " << qpn << ": " << TT << std::endl;
+        
+	hscd_firing_state foo() {
+          if ( in_getTT[0] == 1 )
+            return start;
+          else {
+            out_getTT[0] = in_getTT[0];
+            return other;
+          }
 	}
+
+        void process(void) {}
 
 public:
 	m_h_sched(sc_module_name name)
-	  : hscd_transact_active_node(name) {
-//		setTokens(out_getTT, 1);
-//		setTokens(in_getTT, 1);
-//		SC_THREAD(process);
-	}
+	  : hscd_choice_passive_node(name,
+              start = Choice(
+                (in_getTT(1) & out_getTT(1)) >> call(&m_h_sched::process, &start) |
+                in_getTT(1) >> branch(&m_h_sched::foo,
+                  &start |
+                  (other = Transact( out_getTT(1) >> call(&m_h_sched::process, &start) ))
+                )
+              ) ) {}
 };
 
 
 class m_h_transmit_queue :
-	public hscd_choice_active_node {
+        public markers,
+	public hscd_transact_passive_node {
 public:
 	hscd_port_in<t_qpn> in_getTT;
 	hscd_port_in<void> in_void;
 	hscd_port_out<t_TT> out_getTT;
-
 private:
-	void process () {
-		t_qpn qpn;
-
-		while(1) {
-		  choice( in_getTT(1) | in_void(1) );
-		  
-		  if ( in_getTT ) {
-		    cout << " TQ> t in\n";
-		    
-		    //qpn = in_getTT.read();
-		    qpn = in_getTT[0];
-		    
-		    cout << "TQ> TT requested for qpn " << qpn << std::endl;
-		    
-		    out_getTT[0] = 7;
-		    transact(out_getTT(1));
-		    cout << " TQ> t out\n";
-		  } else {
-		    std::cout << "foo" << std::endl;
-		  }
-		}
-
+	hscd_firing_state process () {
+          if ( in_getTT[0] == 1 )
+            return start;
+          else {
+            out_getTT[0] = in_getTT[0];
+            return other;
+          }
 	}
-
+        
+	void nothing() {}
 public:
 	m_h_transmit_queue(sc_module_name name)
-	  : hscd_choice_active_node(name) {
-//		setTokens(out_getTT, 1);
-//		setTokens(in_getTT, 1);
-//		SC_THREAD(process);
-	}
+	  : hscd_transact_passive_node( name,
+              start = Transact(
+                in_getTT(1) >> branch(&m_h_transmit_queue::process,
+                  &start |
+                  (other = Transact( out_getTT(1) >> call(&m_h_transmit_queue::nothing, &start) ))
+                )
+              )
+            ) {}
 };
 
 class m_h_foo:
-	public hscd_fixed_transact_active_node {
+	public hscd_fixed_transact_passive_node {
 public:
 	hscd_port_in<t_qpn> in_getTT;
 	hscd_port_out<t_TT> out_getTT;
 private:
 	void process () {
-	  t_qpn qpn;
-	  
-	  while(1) {
-	    out_getTT[0] = in_getTT[0];
-	    transact();
-	  }
+          out_getTT[0] = in_getTT[0];
 	}
 public:
-	m_h_foo()
-	  :hscd_fixed_transact_active_node( "foo", in_getTT(1) & out_getTT(1) ) {}
+	m_h_foo(sc_module_name name)
+	  :hscd_fixed_transact_passive_node( name,
+              in_getTT(1) & out_getTT(1) ) {
+        }
 };
 
+/*
 class m_top
 : public hscd_fifocsp_structure {
   private:
@@ -133,10 +125,11 @@ class m_top
       asap = new hscd_scheduler_asap( "asap", getNodes() );
     }
 };
+*/
 
 int sc_main (int argc, char **argv) {
-  hscd_top top( new m_top("top") );
-  
-  sc_start(-1);
+//  hscd_top top( new m_top("top") );
+  m_h_sched sched("sched");
+//  sc_start(-1);
   return 0;
 }
