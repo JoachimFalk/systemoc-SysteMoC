a68ef22d6ef0adaa63c595cd9f47951eed6941f8
Joachim Falk <falk@cs.fau.de>
2005-02-07 10:18:19 +0000

    hscd_graph_sdf => hscd_graph_dataflow in hscd_structure.hpp
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SysteMoC--mainline--0.2--patch-13

--- hscd_structure.hpp
+++ hscd_structure.hpp
@@ -12,34 +12,34 @@
 #include <map>
 
 class hscd_sdf_structure
-  : public hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>,
+  : public hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>,
     public hscd_fixed_transact_node {
 public:
   typedef hscd_sdf_structure	this_type;
 public:
   hscd_sdf_structure( sc_module_name name )
-    : hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>(name),
+    : hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>(name),
       hscd_fixed_transact_node( hscd_op_transact() ) {}
   
   template <typename T_value_type>
   void connectNodePorts( hscd_port_out<T_value_type> &a,
                          hscd_port_in<T_value_type>  &b,
                          hscd_fifo<T_value_type>      i = hscd_fifo<T_value_type>() ) {
-    hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::
+    hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::
       connectNodePorts(a,b,i);
   }
   template <int n, typename T_value_type>
   void connectNodePorts( hscd_port_out<T_value_type> &a, hscd_port_in<T_value_type> &b ) {
-    hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::
+    hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::
       connectNodePorts(a,b,hscd_fifo<T_value_type>(n));
   }
   
 #ifndef __SCFE__
   void assemble( hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::assemble(pgw);
+    return hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::assemble(pgw);
   }
   void pgAssemble( sc_module *m, hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::pgAssemble(pgw);
+    return hscd_graph_dataflow<hscd_fixed_transact_node, hscd_fifo_kind, hscd_fifo>::pgAssemble(pgw);
   }
 #endif
 protected:
@@ -47,32 +47,32 @@ protected:
 };
 
 class hscd_fifocsp_structure
-  : public hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>,
+  : public hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>,
     public hscd_choice_node {
 public:
   typedef hscd_fifocsp_structure	this_type;
 public:
   hscd_fifocsp_structure( sc_module_name name )
-    : hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>(name),
+    : hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>(name),
       hscd_choice_node() {}
   
   template <typename T_value_type>
   void connectNodePorts( hscd_port_out<T_value_type> &a, hscd_port_in<T_value_type> &b ) {
-    hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::
+    hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::
       connectNodePorts(a,b,hscd_fifo<T_value_type>());
   }
   template <int n, typename T_value_type>
   void connectNodePorts( hscd_port_out<T_value_type> &a, hscd_port_in<T_value_type> &b ) {
-    hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::
+    hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::
       connectNodePorts(a,b,hscd_fifo<T_value_type>(n));
   }
 
 #ifndef __SCFE__
   void assemble( hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::assemble(pgw);
+    return hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::assemble(pgw);
   }
   void pgAssemble( sc_module *m, hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::pgAssemble(pgw);
+    return hscd_graph_dataflow<hscd_choice_node, hscd_fifo_kind, hscd_fifo>::pgAssemble(pgw);
   }
 #endif
 protected:
@@ -80,27 +80,27 @@ protected:
 };
 
 class hscd_csp_structure
-  : public hscd_graph_sdf<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>,
+  : public hscd_graph_dataflow<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>,
     public hscd_choice_node {
 public:
   typedef hscd_csp_structure	this_type;
 public:
   hscd_csp_structure( sc_module_name name )
-    : hscd_graph_sdf<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>(name),
+    : hscd_graph_dataflow<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>(name),
       hscd_choice_node() {}
   
   template <typename T_value_type>
   void connectNodePorts( hscd_port_out<T_value_type> &a, hscd_port_in<T_value_type> &b ) {
-    hscd_graph_sdf<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::
+    hscd_graph_dataflow<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::
       connectNodePorts(a,b,hscd_rendezvous<T_value_type>());
   }
   
 #ifndef __SCFE__
   void assemble( hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::assemble(pgw);
+    return hscd_graph_dataflow<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::assemble(pgw);
   }
   void pgAssemble( sc_module *m, hscd_modes::PGWriter &pgw ) const {
-    return hscd_graph_sdf<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::pgAssemble(pgw);
+    return hscd_graph_dataflow<hscd_choice_node, hscd_rendezvous_kind, hscd_rendezvous>::pgAssemble(pgw);
   }
 #endif
 protected:
