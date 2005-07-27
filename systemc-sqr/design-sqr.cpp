3217b3015a7ea7bbf1e1b1885ac3d09c7c3e46dd
Joachim Falk <falk@cs.fau.de>
2005-07-27 07:54:42 +0000

    * updated design-sqr.cpp to use new SysteMoC syntax
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SystemC-Examples--mainline--0.2--patch-28

--- systemc-sqr/design-sqr.cpp
+++ systemc-sqr/design-sqr.cpp
@@ -12,7 +12,7 @@
 # include <smoc_pggen.hpp>
 #endif
 
-class m_h_src: public smoc_fixed_transact_passive_node {
+class m_src: public smoc_actor {
 public:
   smoc_port_out<double> out;
 private:
@@ -22,90 +22,104 @@ private:
     std::cout << "src: " << i << std::endl;
     out[0] = i++;
   }
+  
+  smoc_firing_state start;
 public:
-  m_h_src(sc_module_name name)
-    : smoc_fixed_transact_passive_node(name,
-        out(1) >> call(&m_h_src::src) ),
-      i(1) {}
+  m_src(sc_module_name name)
+    : smoc_actor(name, start), i(1)
+    { start = out(1)            >>
+              call(&m_src::src) >> start; }
 };
 
-class m_h_approx: public smoc_fixed_transact_passive_node {
+class m_approx: public smoc_actor {
 public:
   smoc_port_in<double>  i1, i2;
   smoc_port_out<double> o1;
 private:
   void approx(void) { o1[0] = (i1[0] / i2[0] + i2[0]) / 2; }
+  
+  smoc_firing_state start;
 public:
-  m_h_approx(sc_module_name name)
-    : smoc_fixed_transact_passive_node(name,
-        (i1(1) && i2(1) && o1(1)) >> call(&m_h_approx::approx) ) {}
+  m_approx(sc_module_name name)
+    : smoc_actor(name, start)
+    { start = (i1(1) && i2(1)) >> o1(1) >>
+              call(&m_approx::approx)   >> start; }
 };
 
-class m_h_dup: public smoc_fixed_transact_passive_node {
+class m_dup: public smoc_actor {
 public:
   smoc_port_in<double>  i1;
   smoc_port_out<double> o1, o2;
 private:
   void dup(void) { o1[0] = o2[0] = i1[0]; }
+  
+  smoc_firing_state start;
 public:
-  m_h_dup(sc_module_name name)
-    : smoc_fixed_transact_passive_node(name,
-        (i1(1) && o1(1) && o2(1)) >> call(&m_h_dup::dup) ) {}
+  m_dup(sc_module_name name)
+    : smoc_actor(name, start)
+    { start = i1(1) >> (o1(1) && o2(1)) >>
+              call(&m_dup::dup)         >> start; }
 };
 
-class m_h_sqrloop: public smoc_transact_passive_node {
+class m_sqrloop: public smoc_actor {
 public:
   smoc_port_in<double>  i1, i2;
   smoc_port_out<double> o1, o2;
 private:
-  smoc_firing_state start;
-  smoc_firing_state ok, again, write;
+  double tmp_i1;
   
-  // action function from FSM defined in constructor
-  const smoc_firing_state &check() {
-    std::cout << "check: " << i1[0] << ", " << i2[0] << std::endl;
-    o1[0] = i1[0]; o2[0] = i2[0];
-    // runtime decission of successor state for FSM defined in constructor
-    return (fabs(i1[0] - i2[0]*i2[0]) < 0.0001)
-      ? ok 
-      : write;
+  // action functions for FSM defined in constructor
+  void store() { tmp_i1 = i1[0]; }
+  void copy1() { o1[0] = tmp_i1; }
+  void copy2() { o1[0] = tmp_i1; o2[0] = i2[0]; }
+  
+  // guard  functions for FSM defined in constructor
+  bool check() const {
+    std::cout << "check: " << tmp_i1 << ", " << i2[0] << std::endl;
+    return fabs(tmp_i1 - i2[0]*i2[0]) < 0.0001;
   }
+  
+  smoc_firing_state start;
+  smoc_firing_state loop;
 public:
-  m_h_sqrloop(sc_module_name name)
-    : smoc_transact_passive_node( name, start ) {
-// state               guards             action       function      successor states
-    start = Transact( (i1(1) && i2(1)) >> branch(&m_h_sqrloop::check, ok | write ) );
-    ok    = Transact( (o1(1) && o2(1)) >>                             start);
-    write = Transact(  o1(1)           >>                             again);
-    again = Transact(  i2(1)           >> branch(&m_h_sqrloop::check, ok | write ) );
+  m_sqrloop(sc_module_name name)
+    : smoc_actor( name, start ) {
+    start = i1(1)                                 >>
+            call(&m_sqrloop::store)               >> loop;
+    loop  = (i2(1) &&  guard(&m_sqrloop::check))  >>
+            (o1(1) && o2(1))                      >>
+            call(&m_sqrloop::copy2)               >> start
+          | (i2(1) && !guard(&m_sqrloop::check))  >>
+            o1(1)                                 >>
+            call(&m_sqrloop::copy1)               >> loop;
   }
 };
 
-class m_h_sink: public smoc_fixed_transact_passive_node {
+class m_sink: public smoc_actor {
 public:
   smoc_port_in<double> in;
 private:
-  int i;
-  
   void sink(void) { std::cout << "sink: " << in[0] << std::endl; }
+  
+  smoc_firing_state start;
 public:
-  m_h_sink(sc_module_name name)
-    : smoc_fixed_transact_passive_node(name,
-        in(1) >> call(&m_h_sink::sink) ) {}
+  m_sink(sc_module_name name)
+    : smoc_actor(name, start)
+    { start = in(1) >> call(&m_sink::sink) >> start; }
 };
 
-class m_h_approx_loop
-: public smoc_ddf_constraintset {
+class m_approx_loop
+: public smoc_ndf_constraintset {
   public:
     smoc_port_in<double>  i1;
     smoc_port_out<double> o1;
   protected:
-    m_h_sqrloop sqrloop;
-    m_h_approx  approx;
-    m_h_dup     dup;
+    m_sqrloop sqrloop;
+    m_approx  approx;
+    m_dup     dup;
   public:
-    m_h_approx_loop( sc_module_name name )
-      : smoc_ddf_constraintset(name),
+    m_approx_loop( sc_module_name name )
+      : smoc_ndf_constraintset(name),
         sqrloop("sqrloop"),
         approx("approx"),
         dup("dup") {
@@ -118,16 +132,16 @@ class m_h_approx_loop
     }
 };
 
-class m_h_top
-: public smoc_ddf_constraintset {
+class m_top
+: public smoc_ndf_constraintset {
   public:
   protected:
-    m_h_src                         src;
-    smoc_ddf_moc<m_h_approx_loop>   al;
-    m_h_sink                        sink;
+    m_src                         src;
+    smoc_ndf_moc<m_approx_loop>   al;
+    m_sink                        sink;
   public:
-    m_h_top( sc_module_name name )
-      : smoc_ddf_constraintset(name),
+    m_top( sc_module_name name )
+      : smoc_ndf_constraintset(name),
         src("src"),
         al("al"),
         sink("sink") {
@@ -137,7 +151,7 @@ class m_h_top
 };
 
 int sc_main (int argc, char **argv) {
-  smoc_top_moc<smoc_ddf_moc<m_h_top> > top("top");
+  smoc_top_moc<smoc_ndf_moc<m_top> > top("top");
   
   sc_start(-1);
   return 0;
