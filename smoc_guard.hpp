d744b4a63f26b65167f5d083dfd96c09cd900e93
Joachim Falk <joachim.falk@gmx.de>
2005-08-12 22:06:44 +0000

    * Removed long obsolete commented out code
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.3--patch-54

--- smoc_guard.hpp
+++ smoc_guard.hpp
@@ -30,78 +30,7 @@ public:
   friend class smoc_firing_state;
 protected:
   Expr::Ex<smoc_root_port_bool>::type guard;
-//  typedef std::list<smoc_guard_ptr> guards_ty;
-//  guards_ty guards;
-//  
-//  template <typename T>
-//  struct filterNot_ty: public T {
-//    bool operator ()( const smoc_guard &port ) const
-//      { return !T::operator ()(port); }
-//  };
-//  
-//  template <typename T>
-//  this_type grep() const {
-//    smoc_activation_pattern retval;
-//    const T filter = T();
-//    
-//    for ( guards_ty::const_iterator iter = guards.begin();
-//	  iter != guards.end();
-//	  ++iter )
-//      if ( filter(**iter) )
-//	retval &= *iter;
-//    return retval;
-//  }
-//  template <typename T>
-//  bool isAnd() const {
-//    const T filter = T();
-//    
-//    for ( guards_ty::const_iterator iter = guards.begin();
-//	  iter != guards.end();
-//	  ++iter )
-//      if ( !filter(**iter) )
-//        return false;
-//    return true;
-//  }
-//  template <typename T>
-//  bool isOr() const { return !isAnd<filterNot_ty<T> >(); }
-//  
-//  struct filterInput_ty {
-//    bool operator ()( const smoc_guard &port ) const
-//      { return port.isInput(); }
-//  };
-//  typedef filterNot_ty<filterInput_ty> filterOutput_ty;
-//  
-//  struct filterUplevel_ty {
-//    bool operator ()( const smoc_guard &port ) const
-//      { return port.isUplevel(); }
-//  };
-//  typedef filterNot_ty<filterUplevel_ty> filterNotUplevel_ty;
-//  
-//  struct filterKnownSatisfiable_ty {
-//    bool operator ()( const smoc_guard &port ) const
-//      { return port.knownSatisfiable(); }
-//  };
-//  typedef filterNot_ty<filterKnownSatisfiable_ty> filterNotKnownSatisfiable_ty;
-//  
-//  struct filterKnownUnsatisfiable_ty {
-//    bool operator ()( const smoc_guard &port ) const
-//      { return port.knownUnsatisfiable(); }
-//  };
-//  typedef filterNot_ty<filterKnownUnsatisfiable_ty> filterNotKnownUnsatisfiable_ty;
-//  
-//  struct filterSatisfied_ty {
-//    bool operator ()( const smoc_op_port &port ) const
-//      { return port.satisfied(); }
-//  };
 public:
-//  this_type onlyInputs() const { return grep<filterInput_ty>(); }
-//  this_type onlyOutputs() const { return grep<filterOutput_ty>(); }
-//  this_type onlyUplevel() const { return grep<filterUplevel_ty>(); }
-//  this_type onlyNotUplevel() const { return grep<filterNotUplevel_ty>(); }
-//  bool      knownSatisfiable() const { return isAnd<filterKnownSatisfiable_ty>(); }
-//  bool      knownUnsatisfiable() const { return isOr<filterKnownUnsatisfiable_ty>(); }
-//  bool      satisfied() const { return isAnd<filterSatisfied_ty>(); }
-  
   smoc_root_port_bool knownSatisfiable() const
     { return  Expr::evalTo<Expr::Value>(guard); }
 //  smoc_root_port_bool knownUnsatisfiable() const
@@ -184,25 +113,5 @@ public:
 static inline
 std::ostream &operator <<( std::ostream &out, const smoc_activation_pattern &ap)
   { ap.dump(out); return out; }
-
-/*
-#ifndef _COMPILEHEADER_SMOC_ACTIVATION_PATTERN__OPERATOR_AND
-GNU89_EXTERN_INLINE
-#endif
-smoc_activation_pattern operator & (
-    const smoc_activation_pattern &ap,
-    const smoc_guard_ptr &p) {
-  return smoc_activation_pattern(ap) &= p;
-}
-
-template <class B>
-class DOpExecute<smoc_activation_pattern, DExpr<B>, DOpBAnd> {
-public:
-  typedef smoc_activation_pattern                 result_type;
-  
-  static inline
-  result_type apply(const smoc_activation_pattern &ap, const DExpr<B> &g)
-    { return result_type(ap) &= g; }
-};*/
 
 #endif // _INCLUDED_SMOC_GUARD_HPP
