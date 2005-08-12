67f58b755b269c9fefa529615719c7117782a564
Martin Streubuehr <martin.streubuehr@web.de>
2005-08-12 19:38:30 +0000

    - implementation of smoc_event_or_list
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.3--patch-52

--- smoc_event.hpp
+++ smoc_event.hpp
@@ -2,7 +2,7 @@
 #include <systemc.h>
 
 #include <vector>
-#include <list>
+#include <set>
 
 #ifndef _INCLUDED_SMOC_EVENT_HPP
 #define _INCLUDED_SMOC_EVENT_HPP
@@ -16,20 +16,19 @@ protected:
 
 class smoc_event {
 public:
-  typedef smoc_event this_type;
+  typedef smoc_event                        this_type;
   typedef void (this_type::*unspecified_bool_type)();
-private:
-  typedef std::list<smoc_event_listener *>  ell_ty;
+protected:
+  typedef std::set<smoc_event_listener *>  ell_ty;
   
-  bool    notified;
+  size_t  missing;
   ell_ty  ell;
 public:
-  void reset()
-  {notified=0;}
-  smoc_event(): notified(false) {}
+  smoc_event(): missing(1)
+    {}
   
   void notify() {
-    notified = true;
+    missing = 0;
     for ( ell_ty::iterator iter = ell.begin();
           iter != ell.end();
           ++iter )
@@ -37,55 +36,137 @@ public:
     ell.clear();
   }
   
-  void addListener(smoc_event_listener *el)
-    { ell.push_back(el); }
+  virtual void addListener(smoc_event_listener *el) {
+    bool success = ell.insert(el).second;
+    assert( success );
+  }
+  virtual void delListener(smoc_event_listener *el) {
+    ell.erase(el);
+  }
+  
+  virtual void reset()
+    { missing = 1; }
   
   operator unspecified_bool_type() const
-    { return notified ? &this_type::notify : NULL; }
+    { return missing == 0 ? &this_type::notify : NULL; }
   
   class smoc_event_or_list  operator | ( smoc_event &p );
   class smoc_event_and_list operator & ( smoc_event &p );
+
+  virtual ~smoc_event() {}
+private:
+  // disable
+  // smoc_event( const this_type & );
 };
 
 class smoc_event_or_list
 : public std::vector<smoc_event *>,
-  public smoc_event {
+  public smoc_event,
+  protected smoc_event_listener {
+protected:
+  void signaled( smoc_event *e )
+    { clearListener(); notify();  }
+  
+  void clearListener() {
+    for ( iterator iter = begin();
+	  iter != end();
+	  ++iter )
+      (*iter)->delListener(this);
+  }
 public:
   typedef smoc_event_or_list this_type;
   
+  smoc_event_or_list()
+    { missing = 0; }
   smoc_event_or_list( smoc_event &p )
     { *this |= p; }
   this_type operator | ( smoc_event &p )
     { return this_type(*this) |= p; }
-  this_type &operator |= ( smoc_event &p )
-    { push_back(&p); return *this; }
+  this_type &operator |= ( smoc_event &p ) {
+    if (p)
+      missing = 0;
+    push_back(&p);
+    return *this;
+  }
+  
+  void addListener(smoc_event_listener *el) {
+    if ( ell.empty() ) {
+      missing = 1;
+      for ( iterator iter = begin();
+	    iter != end();
+	    ++iter ) {
+	if ( !**iter ) {
+	  (*iter)->addListener(this);
+	} else {
+	  missing = 0;
+	}
+      }
+    }
+    smoc_event::addListener(el);
+    if ( !missing )
+      notify();
+  }
+  void reset() {
+    missing = 0;
+    for ( iterator iter = begin();
+	  iter != end();
+	  ++iter ) {
+      ++missing;
+      (*iter)->reset();
+    }
+  }
+
 };
 
 class smoc_event_and_list
 : public std::vector<smoc_event *>,
   public smoc_event,
   protected smoc_event_listener {
-private:
-  size_t missing;
-public:
-  typedef smoc_event_and_list this_type;
-  
-  smoc_event_and_list( smoc_event &p ): missing(0)
-    { *this &= p; }
-  this_type operator & ( smoc_event &p )
-    { return this_type(*this) &= p; }
-  this_type &operator &= ( smoc_event &p ) {
-    if ( !p ) {
-      p.addListener(this); 
-      ++missing;
-    }
-    push_back(&p);
-    return *this;
-  }
+protected:
   void signaled( smoc_event *e ) {
     assert( missing > 0 );
     if ( !--missing )
       notify();
+  }
+public:
+  typedef smoc_event_and_list this_type;
+  
+  smoc_event_and_list()
+    { missing = 0; }
+  smoc_event_and_list( smoc_event &p )
+    { missing = 0; *this &= p; }
+  this_type operator & ( smoc_event &p )
+    { return this_type(*this) &= p; }
+  this_type &operator &= ( smoc_event &p ) {
+    if ( !p )
+      ++missing;
+    push_back(&p);
+    return *this;
+  }
+  
+  void addListener(smoc_event_listener *el) {
+    if ( ell.empty() ) {
+      missing = 0;
+      for ( iterator iter = begin();
+	    iter != end();
+	    ++iter )
+	if ( !**iter ) {
+	  ++missing;
+	  (*iter)->addListener(this);
+	}
+    }
+    smoc_event::addListener(el);
+    if ( !missing )
+      notify();
+  }
+  void reset() {
+    missing = 0;
+    for ( iterator iter = begin();
+	  iter != end();
+	  ++iter ) {
+      ++missing;
+      (*iter)->reset();
+    }
   }
   
   virtual ~smoc_event_and_list() {}
@@ -105,22 +186,19 @@ void smoc_notify(smoc_event& se)
 static inline
 void smoc_reset(smoc_event& se)
   { return se.reset(); }
-
 static inline
 void smoc_wait( smoc_event &se ) {
   if ( !se ) {
     struct _: public smoc_event_listener {
       sc_event e;
       void signaled( smoc_event * )
-        { e.notify(); }
+      { e.notify(); }
       virtual ~_() {}
     } w;
     se.addListener(&w);
     wait(w.e);
   }
 }
-  
-
 
 #endif // _INCLUDED_SMOC_EVENT_HPP
 
