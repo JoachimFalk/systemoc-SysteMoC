ba2de48c0350bf0b475557f400f3843db4a83961
Joachim Falk <falk@cs.fau.de>
2005-08-13 01:55:47 +0000

    * Now provide vpc_event in smoc_actor class for later usage of VPC coupling
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.3--patch-58

--- smoc_node_types.hpp
+++ smoc_node_types.hpp
@@ -14,17 +14,24 @@ class smoc_actor
   protected:
     explicit smoc_actor( sc_module_name name, const smoc_firing_state &s )
       : smoc_root_node(s),
-        sc_module(name) {}
+        sc_module(name),
+        vpc_event(true){}
     smoc_actor(const smoc_firing_state &s)
       : smoc_root_node(s),
-        sc_module( sc_gen_unique_name("smoc_actor") ) {}
+        sc_module( sc_gen_unique_name("smoc_actor") ),
+        vpc_event(true) {}
     explicit smoc_actor( sc_module_name name, smoc_firing_state &s )
       : smoc_root_node(s),
-        sc_module(name) {}
+        sc_module(name),
+        vpc_event(true) {}
     smoc_actor(smoc_firing_state &s)
       : smoc_root_node(s),
-        sc_module( sc_gen_unique_name("smoc_actor") ) {}
+        sc_module( sc_gen_unique_name("smoc_actor") ),
+        vpc_event(true) {}
   public:
+  // FIXME: protection
+  smoc_event      vpc_event;
+  
 #ifndef __SCFE__
     sc_module *myModule() { return this; }
     
