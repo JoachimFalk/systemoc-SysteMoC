31ecedd8efaad555b38c58f3e93b353c4d0b201a
Joachim Falk <falk@cs.fau.de>
2005-09-13 16:08:03 +0000

    * Added subdirectory systemc-mpeg4-teich-v2 for Prof. Teich - systemc-mpeg4-teich-v2 should be used to test the xslt converter - The toplevel design is constructed in systemc-mpeg4-teich-v2/design-mpeg4-teich.cpp - The xslt generated code belongs into systemc-mpeg4-teich-v2/Video_XML - The code in Video_XML should be included in the design-mpeg4-teich.cpp file - The network graph of the actors should be constructed in design-mpeg4-teich.cpp an example network graph for the an IDCT2d test is included in the design-mpeg4-teich.cpp file.
    Keywords: 
    
    
    
    
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SystemC-Examples--mainline--0.2--patch-45

---
+++ systemc-mpeg4-teich-v2/callib.hpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <iostream>
+
+#ifndef _INCLUDED_CALLIB_HPP
+#define _INCLUDED_CALLIB_HPP
+
+template <typename T>
+struct cal_list {
+  typedef std::vector<T> t;
+};
+
+cal_list<int>::t Integers(int s, int e) {
+  cal_list<int>::t retval;
+  
+  for ( int i = s; i <= e; ++i )
+    retval.push_back(i);
+  return retval;
+}
+
+template <typename T>
+T cal_bitand( T a, T b ) { return a & b; }
+
+template <typename T>
+T cal_bitor( T a, T b ) { return a | b; }
+
+template <typename T>
+T cal_bitxor( T a, T b ) { return a ^ b; }
+
+#define rshift cal_rshift
+template <typename T1>
+T1 cal_rshift( T1 value, unsigned int factor ) { return value >> factor; }
+
+template <typename T1>
+T1 cal_lshift( T1 value, unsigned int factor ) { return value << factor; }
+
+template <typename T>
+std::ostream &operator << ( std::ostream &o, const std::vector<T> &l ) {
+  o << "[" << std::endl;
+  
+  int index = 0;
+  for ( typename cal_list<T>::t::const_iterator iter = l.begin();
+        iter != l.end();
+        ++iter, ++index )
+    o << "  " << index << " => " << (*iter) << std::endl;
+  o << "]";
+  return o;
+}
+
+/*
+template <typename T1, typename F, typename T2>
+cal_list<T1>::t map( const cal_list<T2>::t &l ) {
+  cal_list<T1>::t retval;
+  
+  for ( cal_list<T2>::t::const_iterator iter = l.begin();
+        iter != l.end();
+        ++iter )
+    retval.push_back( F::apply(*iter) );
+  return retval;
+}*/
+
+#endif // _INCLUDED_CALLIB_HPP
