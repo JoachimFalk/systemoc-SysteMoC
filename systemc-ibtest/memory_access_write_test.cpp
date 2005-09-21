55f2b3b8376afd9521a22654ca2dc2b6e8873bd1
Joachim Falk <falk@cs.fau.de>
2005-09-21 10:08:54 +0000

    * Merged memory interface modelling for IBM HCA from SystemC-Examples--mainline--0.3 Branch
    Patches applied:
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SystemC-Examples--mainline--0.3--patch-36
       
     - completed memory interfaces (write and read) and test cases
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SystemC-Examples--mainline--0.3--patch-37
       
     - changed filename to memory_interface.h
    
    
    git-archimport-id: joachim.falk@gmx.de--2004-systemc-stuff/SystemC-Examples--mainline--0.2--patch-51

--- systemc-ibtest/memory_access_write_test.cpp
+++ systemc-ibtest/memory_access_write_test.cpp
@@ -14,20 +14,20 @@ using std::cout;
 
 class m_source: public smoc_actor {
 public:
-  smoc_port_out<data_request>   out;
+  smoc_port_out<data_buffer>   out;
 private: 
   int times;
   smoc_firing_state start;
-  bool oneTime() const{
+  bool xTimes() const{
     return (times!=0);
   }
   void action(){
-    out[0]=data_request(times, times*20, 24);
+    out[0]=data_buffer("123456789abcdefghijkl", times, 20*times);
     times--;
   }
 public:
   m_source(sc_module_name name):smoc_actor(name,start),times(2){
-    start= (out(1) && guard(&m_source::oneTime) )>> CALL(m_source::action) >> start;
+    start= (out(1) && guard(&m_source::xTimes) )>> CALL(m_source::action) >> start;
   }
  
 };
@@ -35,11 +35,11 @@ public:
 
 class m_sink: public smoc_actor {
 public:
-  smoc_port_in<data_buffer>   in;
+  smoc_port_in<storage_completed_msg>   in;
 private: 
   smoc_firing_state start;
   void action(){
-    cerr << "SINK> got data_buffer: " << in[0].data<< endl;
+    cerr << "SINK> got storage_completed_msg: " << in[0].buffer_id<< endl;
   }
 public:
   m_sink(sc_module_name name):smoc_actor(name,start){
@@ -59,13 +59,13 @@ public:
       char buf1[512]="a0a1a2a3a4a5a6a7a8a9b0b1b2b3b4b5b6b78b9c0c1c23c4c5c6c78c9d0d1d2d3d4d5d6d7d8d9e0e1e2e3e4e5e6e7e8e9f0f1f2f3f4f5f6f7f8f9g0g1g2g3g4g5g6g7g8g9h0h1h2h3h4h5h6h7h8h9i0i1i2i3i4i5i6i7i8i9j0j1j2j3j45j6j7j8j9k0k12k3k4k5k6k7k8k9l0l1l2l3l4l5l6l7l8l9m0m1m2m3m4m5m6m7m8m9n0n1n2n3n4n5n6n7n8n";
       char buf2[512]="0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
       
-      memory_access_read<16>  &read   = registerNode(new memory_access_read<16>("read",buf1));
-      //memory_access_write<16> &write  = registerNode(new memory_access_write<16>("write",buf2));
+      //memory_access_read<16>  &read   = registerNode(new memory_access_read<16>("read",buf1));
+      memory_access_write<16> &write  = registerNode(new memory_access_write<16>("write",buf2));
 
       m_source                &source = registerNode(new m_source("source"));
       m_sink                  &sink   = registerNode(new m_sink("sink"));
-      connectNodePorts( source.out,  read.in );  
-      connectNodePorts( read.out, sink.in ); 
+      connectNodePorts( source.out,  write.in );  
+      connectNodePorts( write.out, sink.in ); 
     
   }
 };
--- vpc-test-h261/c_in_bm_id106017.cpp
+++ vpc-test-h261/c_in_bm_id106017.cpp
@@ -9,10 +9,10 @@ while(true){
 bool var_in2c_in_bm_id106535 = port_in2c_in_bm_id106535->read();
 bool var_c_in_bm2bm_id106536 = 0;
 AbstractComponent& r=Director::getInstance().getResource(C_IN_BM_ID106017);
-smoc_event *ev=new smoc_event(); 
-r.compute(C_IN_BM_ID106017,"",ev);
-smoc_wait(*(ev));
-smoc_reset(*(ev));
+//smoc_event *ev=new smoc_event(); 
+r.compute(C_IN_BM_ID106017,"");
+//smoc_wait(*(ev));
+//smoc_reset(*(ev));
 cout << "C_IN_BM triggered!" << endl << endl;
 port_c_in_bm2bm_id106536->write(var_c_in_bm2bm_id106536);
 }
