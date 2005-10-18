df8274cbf3ad6f879f44b07adc35eab1439910ad
Martin Streubuehr <martin.streubuehr@web.de>
2005-10-18 08:00:17 +0000

    - add "till test" in systemc-td-sim-test
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SystemC-Examples--mainline--0.3--patch-68

---
+++ systemc-td-sim-test/till_test.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <smoc_moc.hpp>
+#include <smoc_port.hpp>
+#include <smoc_fifo.hpp>
+#include <smoc_node_types.hpp>
+#include <smoc_event.hpp>
+
+class till_actor: public smoc_actor{
+
+private:
+  smoc_firing_state main;
+  
+  /**
+   *
+   */
+  void process(){
+    cerr << "Got timeout at: " << sc_time_stamp() << endl;
+    smoc_reset(timeout);
+  }
+public:
+
+  smoc_event timeout;
+
+  till_actor( sc_module_name name ) : smoc_actor ( name , main ){
+    main = Expr::till( timeout )
+      >> CALL(till_actor::process)
+      >> main
+      ;
+  }
+};
+
+class till_top: public smoc_graph {
+  SC_HAS_PROCESS(till_top);
+protected:
+  till_actor act;
+  void time_out_process() {
+    while(1){
+      wait(120, SC_NS);
+      smoc_notify(act.timeout);
+    }
+  }
+public:
+  till_top( sc_module_name name ) : smoc_graph( name ), act(till_actor("till_act")){
+    registerNode(&act);
+    SC_THREAD(time_out_process);
+  }
+};
+
+int sc_main (int argc, char **argv) {
+  smoc_top_moc<till_top> top("top");
+  
+  sc_start(-1);
+  return 0;
+}
