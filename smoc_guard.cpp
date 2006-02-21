9bfaebbe06f787ef94b40138e1c69462b629f35c
Joachim Falk <falk@cs.fau.de>
2006-02-21 10:11:06 +0000

    * Moved smoc_activation_pattern::guardAssemble helper function from smoc_guard.hpp into smoc_guard.cpp
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.3--patch-129

--- smoc_guard.cpp
+++ smoc_guard.cpp
@@ -18,9 +18,91 @@
 
 #include <smoc_guard.hpp>
 
+#include <typeinfo>
+
 void smoc_activation_pattern::dump(std::ostream &out) const {
 //  for ( guards_ty::const_iterator iter = guards.begin();
 //        iter != guards.end();
 //        ++iter )
 //    out << " " << **iter;
 }
+
+void smoc_activation_pattern::guardAssemble(
+    smoc_modes::PGWriter &pgw, const Expr::PASTNode &n ) {
+  pgw.indentUp();
+  do {
+    if (n->isa<Expr::ASTNodeBinOp>()) {
+   
+      Expr::PASTNodeBinOp p = n->isa<Expr::ASTNodeBinOp>();
+      pgw << "<ASTNodeBinOp OpType=\"" << p->getOpType() <<"\" >" << std::endl;
+      
+      pgw.indentUp();
+      pgw << "<lhs>" << std::endl;
+      guardAssemble(pgw, p->getLeftNode());
+      pgw << "</lhs>" << std::endl;
+      
+      pgw << "<rhs>" << std::endl;
+      guardAssemble(pgw, p->getRightNode());
+      pgw << "</rhs>" << std::endl;
+      
+      pgw.indentDown();
+      pgw << "</ASTNodeBinOp>" << std::endl;
+    
+    } else if ( n->isa<Expr::ASTNodeUnOp>()) {
+      
+      Expr::PASTNodeUnOp p = n->isa<Expr::ASTNodeUnOp>();
+      pgw << "<ASTNodeUnOp OpType=\"" << p->getOpType() <<"\" >" << std::endl;
+      
+      pgw.indentUp();
+      
+      pgw << "<ChildNode>" << std::endl;
+      guardAssemble(pgw, p->getChildNode());
+      pgw << "</ChildNode>" << std::endl;
+      
+      pgw.indentDown();
+      pgw << "</ASTNodeUnOp>" << std::endl;
+    
+    } else { 
+      //***********here is Terminal************
+      //assert( n->isa<Expr::ASTNodeTerminal>() );
+      if ( n->isa<Expr::ASTNodePortTokens>() ) {
+        pgw << "<PortTokens portid=\""
+              << pgw.getId(n->isa<Expr::ASTNodePortTokens>()->getPort())
+              << "\"/>" << std::endl;
+        //pgw << "</PortTokens>" << std::endl;
+      } else if ( n->isa<Expr::ASTNodeLiteral>() ) {
+        pgw << "<Literal "
+                 "value=\"" << n->isa<Expr::ASTNodeLiteral>()->value << "\">"
+//               "type=\""  << "typeid(xxx).name()" << "\"/>"
+            << std::endl;
+        //pgw << "</Literal>" << std::endl;
+      } else if ( n->isa<Expr::ASTNodeVar>() ) {
+        pgw << "<Var name=\"" << std::hex <<
+          n->isa<Expr::ASTNodeVar>()->getName() <<  "\"/>" << std::endl;
+        //pgw << "</Var>" << std::endl;
+      } else if ( n->isa<Expr::ASTNodeProc>() ) {
+        pgw << "<Proc 0x = \"" << std::hex << reinterpret_cast<unsigned long>
+          (n->isa<Expr::ASTNodeProc>()->ptrProc()) << "\"/>" << std::endl;
+        //pgw << "</Proc>" << std::endl;
+      } else if ( n->isa<Expr::ASTNodeMemGuard>() ) {
+        pgw << "<MemGuard objPtr=\"0x" << std::hex << reinterpret_cast<unsigned long>
+          (n->isa<Expr::ASTNodeMemGuard>()->ptrObj()) << "\" name=\"" <<
+          (n->isa<Expr::ASTNodeMemGuard>()->getName()) << "\"/>" << std::endl;
+        //pgw << "</MemGuard>" << std::endl;
+      }  else if ( n->isa<Expr::ASTNodeMemProc>() ) {
+        pgw << "<MemProc "
+                 "objPtr=\"0x" << std::hex << reinterpret_cast<unsigned long>
+                   (n->isa<Expr::ASTNodeMemProc>()->ptrObj()) << "\" "
+                 "addrPtr=\"0x" << std::hex << *reinterpret_cast<const unsigned long *>
+                   (&n->isa<Expr::ASTNodeMemProc>()->ptrMemProc()) << "\"/>"
+            << std::endl;
+      } else if ( n->isa<Expr::ASTNodeToken>() ) {
+        pgw << "<Token FIXME !!!/>" << std::endl;
+      } else {
+        pgw << "<Unkown Terminal FIXME !!!/>" << std::endl;
+      }
+    }
+  } while (0);
+  pgw.indentDown();
+}
+
