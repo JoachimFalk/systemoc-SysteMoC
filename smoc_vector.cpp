2527355ff321fece20177ad7b0563150329e61a7
Joachim Keinert <keinert@codesign.informatik.uni-erlangen.de>
2006-11-10 19:28:02 +0000

    - Add functionality for vector initialization
    git-archimport-id: falk@cs.fau.de--2006-systemoc-stuff/SysteMoC--wsdf--0.5--patch-20

---
+++ smoc_vector.cpp
@@ -0,0 +1,8 @@
+
+#include <smoc_vector.hpp>
+
+namespace smoc_vector_init {
+
+	smoc_vector_init<smoc_vector_cs<int,1>::size_type > vector_init;
+
+}
--- smoc_vector.hpp
+++ smoc_vector.hpp
@@ -6,6 +6,99 @@
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/storage.hpp>
 
+
+
+/* ************************************************************* */
+/*                      Assembling of a vector                   */
+/* ************************************************************* */
+
+namespace smoc_vector_init {
+
+
+	///Special initialization class
+	template <typename ID_TYPE>
+	class smoc_vector_init_base
+	{
+		
+	public:
+		typedef boost::numeric::ublas::vector<ID_TYPE> vector_type;
+		
+	public:
+		
+		const ID_TYPE& operator()(const unsigned idx) const{
+			return vector[idx];
+		}
+		
+		const vector_type& operator()() const{
+			return vector;
+		}
+		
+	protected:
+		vector_type vector;	
+	};
+	
+	//N1: current dimension (indexed with zero)
+	//Note: The maximum number of dimensions is not a template parameter as previously
+	//intended. The reason is, that then, we require a special vector init for each
+	//possible dimension.
+	template <typename ID_TYPE, 
+						int N1 = 0>
+	class smoc_vector_init
+		: public smoc_vector_init_base<ID_TYPE>
+	{
+	public:
+		// Note: unfortunately, here we cannot return
+		// a reference. Otherwise, a C++ line cannot have more
+		// then one vector init, because we do not have any influence on
+		// the invocation order.
+		// Suppose, that we have 
+		// test_vector3 << vector_init[1][2][3][4][5] << vector_init[10][11][12][13][14];
+		// Then the compiler can call the both vector_inits before calling the <<-operators
+		// (infact, it does!). In this case we would obtain an error when returning a reference.
+		smoc_vector_init<ID_TYPE, N1+1>
+		operator[](const ID_TYPE& id){
+			std::cout << "smoc_vector_init[]: N1 = " << N1 << " id = " << id << std::endl;
+			return smoc_vector_init<ID_TYPE, N1+1>(*this, id);
+		}
+
+
+	public:
+
+		smoc_vector_init<ID_TYPE,N1>()
+			: smoc_vector_init_base<ID_TYPE>(){};
+
+		
+		///special copy constructor required by operator[]
+		smoc_vector_init<ID_TYPE,N1>(const smoc_vector_init<ID_TYPE,N1-1>& i, 
+																 const ID_TYPE& id)
+			: smoc_vector_init_base<ID_TYPE>(i)
+		{
+			(*this).vector.resize(N1);
+			(*this).vector[N1-1] = id;
+		}
+		
+	};
+
+};
+
+
+
+template <class T, class A>
+boost::numeric::ublas::vector<T,A>& 
+operator<<(boost::numeric::ublas::vector<T,A>& input_vector, 
+					 const T& data_el){
+	typename boost::numeric::ublas::vector<T,A>::size_type old_size(input_vector.size());
+
+	input_vector.resize(old_size+1);
+	input_vector[old_size] = data_el;
+
+	return input_vector;
+}
+
+
+/* ************************************************************* */
+/*                          smoc_vector                          */
+/* ************************************************************* */
 template<class T, class A = boost::numeric::ublas::unbounded_array<T> >
 class smoc_vector 
   : public boost::numeric::ublas::vector<T,A>
@@ -67,6 +160,18 @@ public:
     }
   }
 
+	BOOST_UBLAS_INLINE
+	template <int N>
+  smoc_vector(const smoc_vector_init::smoc_vector_init<size_type,N>& 
+							vector_init)
+    : parent_type(N)
+  {
+    for(size_type i = 0; i < N; i++){
+      (*this)[i] = vector_init(i);
+    }
+  }	
+
+
 public:
 	/// Lexicographic comparison
 	bool is_lex_smaller_than(const this_type& vec) const {
@@ -94,6 +199,61 @@ public:
 	}
 
 };
+
+
+/* ************************************************************* */
+/*                     vector with constant size                 */
+/* ************************************************************* */
+template<class T, int N >
+class smoc_vector_cs
+	: public smoc_vector<T,boost::numeric::ublas::bounded_array<T,N> >
+{
+
+public:
+
+	typedef smoc_vector_cs<T,N> this_type;
+  typedef smoc_vector<T,boost::numeric::ublas::bounded_array<T,N> > parent_type;
+  typedef typename parent_type::size_type size_type;
+	
+public:
+
+	BOOST_UBLAS_INLINE
+  smoc_vector_cs()
+    : parent_type(N)
+  {}	
+	
+  BOOST_UBLAS_INLINE
+  smoc_vector_cs(const smoc_vector_init::smoc_vector_init<size_type,N>& 
+								 vector_init)
+    : parent_type(N)
+  {
+    for(size_type i = 0; i < N; i++){
+      (*this)[i] = vector_init(i);
+    }
+  }	
+
+};
+
+
+namespace smoc_vector_init{
+	extern smoc_vector_init<smoc_vector_cs<int,1>::size_type > vector_init;
+};
+
+
+template <class VECTOR_CLASS, class A, typename ID_TYPE, int N>
+boost::numeric::ublas::vector<VECTOR_CLASS, A>& 
+operator<<(boost::numeric::ublas::vector<VECTOR_CLASS,A>& input_vector, 
+					 const smoc_vector_init::smoc_vector_init<ID_TYPE,N>& vector_init){
+	typename boost::numeric::ublas::vector<VECTOR_CLASS,A>::size_type old_size(input_vector.size());
+
+	std::cout << "input_vector = " << input_vector << std::endl;
+	std::cout << "vector_init = " << VECTOR_CLASS(vector_init) << std::endl;
+
+	input_vector.resize(old_size+1);
+	input_vector[old_size] = VECTOR_CLASS(vector_init);
+
+	return input_vector;
+}
 
 
 #endif
