615b3b77a29c59696355bf8e0afcb67ebbb3085e
Joachim Keinert <keinert@codesign.informatik.uni-erlangen.de>
2006-12-15 17:01:32 +0000

    - Add backward dilatation
    git-archimport-id: falk@cs.fau.de--2006-systemoc-stuff/SystemC-Examples--wsdf--0.5--patch-19

--- systemoc-wsdf-recons-dila/design-wsdf-recons-dila-fast.cpp
+++ systemoc-wsdf-recons-dila/design-wsdf-recons-dila-fast.cpp
@@ -22,9 +22,11 @@
 #include "wsdf_img_src.hpp"
 #include "wsdf_2D_dup2.hpp"
 #include "wsdf_2D_dup3.hpp"
-#include "wsdf_2D_dup4.hpp"
 #include "wsdf_1dil_fast.hpp"
 #include "wsdf_output_switch.hpp"
+#include "wsdf_dil_in_switch.hpp"
+#include "wsdf_dil_out_switch.hpp"
+#include "wsdf_rot180.hpp"
 
 #define CAPTURE_DILATATION
 //#define LARGE_BUFFER_SIZE
@@ -54,10 +56,25 @@ public:
 
 		m_wsdf_2D_dup2<T> &seed_dup = 
 			registerNode(new m_wsdf_2D_dup2<T>("seed_dup", size_x, size_y));
-		m_wsdf_2D_dup2<T> &input_dup = 
-			registerNode(new m_wsdf_2D_dup2<T>("input_dup", size_x, size_y));
-		m_wsdf_2D_dup4<T> &min_image_dup = 
-			registerNode(new m_wsdf_2D_dup4<T>("min_image_dup", size_x, size_y));
+		m_wsdf_2D_dup3<T> &input_dup = 
+			registerNode(new m_wsdf_2D_dup3<T>("input_dup", size_x, size_y));
+		m_wsdf_2D_dup3<T> &min_image_dup = 
+			registerNode(new m_wsdf_2D_dup3<T>("min_dup", size_x, size_y));
+		m_wsdf_2D_dup2<T> &dil_dup = 
+			registerNode(new m_wsdf_2D_dup2<T>("dil_dup", size_x, size_y));
+
+		m_wsdf_dil_in_switch<T> &seed_rot_switch =
+			registerNode(new m_wsdf_dil_in_switch<T>("seed_rot_switch",size_x,size_y));
+		m_wsdf_dil_in_switch<T> &input_rot_switch =
+			registerNode(new m_wsdf_dil_in_switch<T>("input_rot_switch",size_x,size_y));
+		m_wsdf_dil_out_switch<T> &dil_switch =
+			registerNode(new m_wsdf_dil_out_switch<T>("dil_switch",size_x,size_y));
+
+		m_wsdf_rot180<T> &input_rot180 =
+			registerNode(new m_wsdf_rot180<T>("input_rot180", size_x,size_y));
+		m_wsdf_rot180<T> &dil_rot180 =
+			registerNode(new m_wsdf_rot180<T>("dil_rot180", size_x,size_y));
+
 		
 		m_wsdf_1dil_fast<T> &dilatation = 
 			registerNode(new m_wsdf_1dil_fast<T>("dilatation", size_x, size_y));
@@ -78,7 +95,7 @@ public:
 
 #ifdef CAPTURE_DILATATION
 		//debug actors
-		m_wsdf_2D_dup2<T> &dil_dup = 
+		m_wsdf_2D_dup2<T> &dil_capture_dup = 
 			registerNode(new m_wsdf_2D_dup2<T>("dilatation_dup", size_x, size_y));
 		m_wsdf_img_snk<T> &dil_sink = registerNode(new m_wsdf_img_snk<T>("dil_sink","dil_out.bmp",size_x,size_y,1000));		
 #endif //CAPTURE_DILATATION
@@ -97,31 +114,46 @@ public:
 #endif
 
 #ifdef LARGE_BUFFER_SIZE
-		indConnectNodePorts(input_dup.out1, min_image.in2, smoc_wsdf_edge<T>(size_y));		
+		indConnectNodePorts(input_dup.out1, input_rot_switch.orig_in, smoc_wsdf_edge<T>(size_y));		
 #else
-		indConnectNodePorts(input_dup.out1, min_image.in2, smoc_wsdf_edge<T>(1));		
+		indConnectNodePorts(input_dup.out1, input_rot_switch.orig_in, smoc_wsdf_edge<T>(1));		
 #endif
-		indConnectNodePorts(input_dup.out2, input_switch.buffered_in, smoc_wsdf_edge<T>(size_y,ns_smoc_vector_init::ul_vector_init[0][size_y]));
+		indConnectNodePorts(input_dup.out2, input_rot180.in, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(input_dup.out3, input_switch.buffered_in, 
+												smoc_wsdf_edge<T>(size_y,ns_smoc_vector_init::ul_vector_init[0][size_y]));
+
+		indConnectNodePorts(input_rot180.out, input_rot_switch.rot_in, smoc_wsdf_edge<T>(size_y));
+
+#ifdef LARGE_BUFFER_SIZE
+		indConnectNodePorts(input_rot_switch.out,min_image.in2,smoc_wsdf_edge<T>(size_y));
+#else
+		indConnectNodePorts(input_rot_switch.out,min_image.in2,smoc_wsdf_edge<T>(1));
+#endif
 		
 #ifdef LARGE_BUFFER_SIZE
-		indConnectNodePorts(seed_dup.out1, dilatation.orig_in, smoc_wsdf_edge<T>(size_y));
-		indConnectNodePorts(seed_dup.out2, diff_image.in1, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(seed_dup.out1, seed_rot_switch.orig_in, smoc_wsdf_edge<T>(size_y));
 #else
-		indConnectNodePorts(seed_dup.out1, dilatation.orig_in, smoc_wsdf_edge<T>(3));
-		indConnectNodePorts(seed_dup.out2, diff_image.in1, smoc_wsdf_edge<T>(3));
+		indConnectNodePorts(seed_dup.out1, seed_rot_switch.orig_in, smoc_wsdf_edge<T>(1));
+#endif
+		indConnectNodePorts(seed_dup.out2, diff_image.in1, smoc_wsdf_edge<T>(size_y));
+
+#ifdef LARGE_BUFFER_SIZE
+		indConnectNodePorts(seed_rot_switch.out, dilatation.orig_in, smoc_wsdf_edge<T>(size_y));
+#else
+		indConnectNodePorts(seed_rot_switch.out, dilatation.orig_in, smoc_wsdf_edge<T>(1));
 #endif
 
 #ifdef CAPTURE_DILATATION
 # ifdef LARGE_BUFFER_SIZE
-		indConnectNodePorts(dilatation.out, dil_dup.in, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(dilatation.out, dil_capture_dup.in, smoc_wsdf_edge<T>(size_y));
 
-		indConnectNodePorts(dil_dup.out1, min_image.in1, smoc_wsdf_edge<T>(size_y));
-		indConnectNodePorts(dil_dup.out2, dil_sink.in, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(dil_capture_dup.out1, min_image.in1, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(dil_capture_dup.out2, dil_sink.in, smoc_wsdf_edge<T>(size_y));
 # else
-		indConnectNodePorts(dilatation.out, dil_dup.in, smoc_wsdf_edge<T>(1));
+		indConnectNodePorts(dilatation.out, dil_capture_dup.in, smoc_wsdf_edge<T>(1));
 
-		indConnectNodePorts(dil_dup.out1, min_image.in1, smoc_wsdf_edge<T>(1));
-		indConnectNodePorts(dil_dup.out2, dil_sink.in, smoc_wsdf_edge<T>(1));
+		indConnectNodePorts(dil_capture_dup.out1, min_image.in1, smoc_wsdf_edge<T>(1));
+		indConnectNodePorts(dil_capture_dup.out2, dil_sink.in, smoc_wsdf_edge<T>(1));
 # endif
 #else
 # ifdef LARGE_BUFFER_SIZE
@@ -138,15 +170,31 @@ public:
 #endif
 
 #ifdef LARGE_BUFFER_SIZE
-		indConnectNodePorts(min_image_dup.out1, diff_image.in2, smoc_wsdf_edge<T>(size_y));
 		indConnectNodePorts(min_image_dup.out2, dilatation.prev_line_in, smoc_wsdf_edge<T>(size_y));
 		indConnectNodePorts(min_image_dup.out3, dilatation.prev_pixel_in, smoc_wsdf_edge<T>(size_y));
 #else
-		indConnectNodePorts(min_image_dup.out1, diff_image.in2, smoc_wsdf_edge<T>(1));
 		indConnectNodePorts(min_image_dup.out2, dilatation.prev_line_in, smoc_wsdf_edge<T>(2));
 		indConnectNodePorts(min_image_dup.out3, dilatation.prev_pixel_in, smoc_wsdf_edge<T>(1));
 #endif
-		indConnectNodePorts(min_image_dup.out4, output_switch.in, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(min_image_dup.out1, dil_rot180.in, smoc_wsdf_edge<T>(size_y));
+
+		indConnectNodePorts(dil_rot180.out, dil_switch.in, smoc_wsdf_edge<T>(size_y));
+
+#ifdef LARGE_BUFFER_SIZE
+		indConnectNodePorts(dil_switch.rot_out, seed_rot_switch.rot_in, smoc_wsdf_edge<T>(size_y));
+		indConnectNodePorts(dil_switch.dil_out, dil_dup.in, smoc_wsdf_edge<T>(size_y));
+#else
+		indConnectNodePorts(dil_switch.rot_out, seed_rot_switch.rot_in, smoc_wsdf_edge<T>(1));
+		indConnectNodePorts(dil_switch.dil_out, dil_dup.in, smoc_wsdf_edge<T>(1));
+#endif
+
+		
+#ifdef LARGE_BUFFER_SIZE
+		indConnectNodePorts(dil_dup.out1, diff_image.in2, smoc_wsdf_edge<T>(size_y));
+#else
+		indConnectNodePorts(dil_dup.out1, diff_image.in2, smoc_wsdf_edge<T>(1));
+#endif
+		indConnectNodePorts(dil_dup.out2, output_switch.in, smoc_wsdf_edge<T>(size_y));
 		
 #ifdef LARGE_BUFFER_SIZE
 		indConnectNodePorts(diff_image.out,image_max.in,smoc_wsdf_edge<T>(size_y));
