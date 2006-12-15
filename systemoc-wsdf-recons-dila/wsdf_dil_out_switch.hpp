58b95a638f5847c2a2a6bb1c1be6bef24ffafc75
Joachim Keinert <keinert@codesign.informatik.uni-erlangen.de>
2006-12-15 17:02:11 +0000

    - Add backward dilatation
    git-archimport-id: falk@cs.fau.de--2006-systemoc-stuff/SystemC-Examples--wsdf--0.5--patch-20

---
+++ systemoc-wsdf-recons-dila/wsdf_dil_out_switch.hpp
@@ -0,0 +1,85 @@
+// vim: set sw=2 ts=8:
+
+#ifndef INCLUDE_WSDF_DIL_OUT_SWITCH_HPP
+#define INCLUDE_WSDF_DIL_OUT_SWITCH_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include <smoc_moc.hpp>
+#include <smoc_md_port.hpp>
+#include <smoc_node_types.hpp>
+#ifndef __SCFE__
+# include <smoc_pggen.hpp>
+#endif
+
+template <typename T>
+class m_wsdf_dil_out_switch: public smoc_actor {
+
+public:
+
+	smoc_md_port_in<T,2> in;
+	
+	smoc_md_port_out<T,2> dil_out;
+	smoc_md_port_out<T,2> rot_out;	
+	
+	
+
+private:
+
+	void copy2dil(){
+		dil_out[0][0] = in[0][0];
+	}
+
+	void copy2rot(){
+		rot_out[0][0] = in[0][0];
+	}
+
+	smoc_firing_state state1;
+	smoc_firing_state state2;
+
+public:
+	m_wsdf_dil_out_switch( sc_module_name name ,
+												 unsigned size_x,
+												 unsigned size_y)
+		: smoc_actor(name,state1),
+			in(ns_smoc_vector_init::ul_vector_init[size_x][size_y], //firing_blocks
+				 ns_smoc_vector_init::ul_vector_init[size_x][size_y], //u0
+				 ns_smoc_vector_init::ul_vector_init[1][1], //c
+				 ns_smoc_vector_init::ul_vector_init[1][1], //delta_c
+				 ns_smoc_vector_init::sl_vector_init[0][0], //bs
+				 ns_smoc_vector_init::sl_vector_init[0][0] //bt
+				 ),
+			dil_out(ns_smoc_vector_init::ul_vector_init[1][1] <<
+							ns_smoc_vector_init::ul_vector_init[size_x][size_y]
+							),
+			rot_out(ns_smoc_vector_init::ul_vector_init[1][1] <<
+							ns_smoc_vector_init::ul_vector_init[size_x][size_y]
+							)
+
+	{
+		state1 = (in(1) && rot_out(1))
+			>> ((in.getIteration(0,0) == (size_t)0) && (in.getIteration(0,1) == (size_t)0))
+			>> CALL(m_wsdf_dil_out_switch::copy2rot) 
+			>> state2
+			| (in(1) && dil_out(1))
+			>> ((in.getIteration(0,0) != (size_t)0) || (in.getIteration(0,1) != (size_t)0))
+			>> CALL(m_wsdf_dil_out_switch::copy2dil) 
+			>> state1;
+
+		state2 = (in(1) && dil_out(1))
+			>> ((in.getIteration(0,0) == (size_t)0) && (in.getIteration(0,1) == (size_t)0))
+			>> CALL(m_wsdf_dil_out_switch::copy2dil) 
+			>> state1
+			| (in(1) && rot_out(1))
+			>> ((in.getIteration(0,0) != (size_t)0) || (in.getIteration(0,1) != (size_t)0))
+			>> CALL(m_wsdf_dil_out_switch::copy2rot) 
+			>> state2;
+
+	}
+	
+};
+
+
+#endif //INCLUDE_WSDF_DIL_OUT_SWITCH_HPP
---
+++ systemoc-wsdf-recons-dila/wsdf_rot180.hpp
@@ -0,0 +1,66 @@
+// vim: set sw=2 ts=8:
+
+#ifndef INCLUDE_WSDF_ROT180_HPP
+#define INCLUDE_WSDF_ROT180_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include <smoc_moc.hpp>
+#include <smoc_md_port.hpp>
+#include <smoc_node_types.hpp>
+#ifndef __SCFE__
+# include <smoc_pggen.hpp>
+#endif
+
+
+///Rotation about 180 degrees
+template <typename T>
+class m_wsdf_rot180: public smoc_actor {
+
+public:
+	
+	smoc_md_port_in<T,2> in;
+	smoc_md_port_out<T,2> out;
+
+private:
+
+	const unsigned size_x;
+	const unsigned size_y;
+
+	void copy_data() {
+		for(unsigned y = 0; y < size_y; y++){
+			for(unsigned x = 0; x < size_x; x++){
+				out[x][y] = in[size_x-x-1][size_y-y-1];
+			}
+		}
+	}
+	smoc_firing_state start;
+	
+public:
+	m_wsdf_rot180( sc_module_name name,
+								 unsigned size_x,
+								 unsigned size_y)
+		: smoc_actor(name,start),
+		in(ns_smoc_vector_init::ul_vector_init[1][1], //firing_blocks
+			 ns_smoc_vector_init::ul_vector_init[size_x][size_y], //u0
+			 ns_smoc_vector_init::ul_vector_init[size_x][size_y], //c
+			 ns_smoc_vector_init::ul_vector_init[1][1], //delta_c
+			 ns_smoc_vector_init::sl_vector_init[0][0], //bs
+			 ns_smoc_vector_init::sl_vector_init[0][0] //bt
+			 ),
+		out(ns_smoc_vector_init::ul_vector_init[size_x][size_y] <<
+				ns_smoc_vector_init::ul_vector_init[size_x][size_y]),
+		size_x(size_x),
+		size_y(size_y)
+	{
+		start = (in(1) && out(1))
+			>> CALL(m_wsdf_rot180::copy_data) 
+			>> start;
+	}
+	
+};
+
+
+#endif //INCLUDE_WSDF_ROT180_HPP
