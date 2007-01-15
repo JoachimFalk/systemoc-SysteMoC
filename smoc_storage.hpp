2043cae072f055450ef8466c27f64d7f23e5151e
Joachim Falk <falk@cs.fau.de>
2007-01-15 17:22:52 +0000

    * Merge from SysteMoC--mainline--0.4 branch - Overload SystemC main with SysteMoC main which checks for --generate-problemgraph (obsolete) and --generate-networkgraph
    Keywords: 
    
    
    
    Patches applied:
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.4--patch-118
       
     * Merge from SysteMoC--mainline--0.5 branch
     - Replace `pwd` with $(top_builddir) for SYSTEMC_VPC_XXX
     - Use CoSupport::RefCountObject instead of _RefCount
       Define BOOST_NO_THREADS for RefCountObject 
     - Minor Makefile.am tweaks
     - Fixed warning
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.4--patch-119
       
     * Merge from SysteMoC--event-scheduler--0.5 branch
     - Fixed SysteMoC to use new oneofTypeid which is defined in
       HscdSupport--mainline--0.3--patch-25. Therefore this SysteMoC
       patchlevel does need at least HscdSupport--mainline--0.3--patch-25.
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.4--patch-121
       
     * Merge from SysteMoC--event-scheduler--0.5 branch
     - Added (C) Hardware-Software-CoDesign to every file added COPYRIGHT
       and LICENSE files
    
     * joachim.falk@gmx.de--2005-systemc-stuff/SysteMoC--mainline--0.4--patch-126
       
     * Overload SystemC main with SysteMoC main which checks for
       --generate-problemgraph (obsolete) and --generate-networkgraph
    
    
    git-archimport-id: falk@cs.fau.de--2006-systemoc-stuff/SysteMoC--event-scheduler--0.5--patch-36

--- smoc_storage.hpp
+++ smoc_storage.hpp
@@ -45,22 +45,14 @@ class smoc_storage
 private:
   char mem[sizeof(T)];
   bool valid;
-
 private:
-  
   T* ptr()
   { return reinterpret_cast<T*>(mem); }
-  
+
   const T* ptr() const
   { return reinterpret_cast<const T*>(mem); }
-  
 public:
   smoc_storage() : valid(false) {
-  }
-  
-  T& get() {
-    assert(valid);
-    return *ptr();
   }
 
   const T& get() const {
@@ -68,16 +60,9 @@ public:
     return *ptr();
   }
 
-  operator T&() {
-    assert(valid);
-    return *ptr();
-  }
+  operator const T&() const
+    { return get(); }
 
-  operator const T&() const {
-    assert(valid);
-    return *ptr();
-  }
-  
   void put(const T &t) {
     if(valid) {
       *ptr() = t;
@@ -86,7 +71,10 @@ public:
       valid = true;
     }
   }
-  
+
+  void operator=(const T& t)
+    { put(t); }  
+
   ~smoc_storage() {
     if(valid) {
       ptr()->~T();
@@ -95,38 +83,71 @@ public:
   }
 };
 
+/// smoc storage with read only memory interface
+// typedef const T & smoc_storage_rom<T>
+
+/// smoc storage with write only memory interface
 template<class T>
-class smoc_write_only_storage
+class smoc_storage_wom
 {
-public:
-  typedef smoc_write_only_storage<T> this_type;
-  
+private:
+  typedef smoc_storage_wom<T> this_type;
 private:
   smoc_storage<T> &s;
-
 public:
-  smoc_write_only_storage(smoc_storage<T> &_s) :
-    s(_s)
-  {}
-  
-  void operator=(const T& t) {
-    s.put(t);
-  }  
+  smoc_storage_wom(smoc_storage<T> &_s)
+    : s(_s) {}
+
+  void operator=(const T& t)
+    { s.put(t); }  
 };
 
+/// smoc storage with read write memory interface
+template<class T>
+class smoc_storage_rw
+{
+private:
+  typedef smoc_storage_wom<T> this_type;
+private:
+  smoc_storage<T> &s;
+public:
+  smoc_storage_rw(smoc_storage<T> &_s)
+    : s(_s) {}
+ 
+  operator const T&() const
+    { return s.get(); }
+
+  void operator=(const T& t)
+    { s.put(t); }  
+};
+
+namespace CoSupport {
+  // provide isType for smoc_storage_rw
+  template <typename T, typename X>
+  static inline
+  bool isType( const smoc_storage_rw<X> &of )
+   { return isType<T>(static_cast<const X &>(of)); }
+};
 
 template<class T>
 struct smoc_storage_in
 {
-  typedef const smoc_storage<T> storage_type;
-  typedef const T &		return_type;
+  typedef const smoc_storage<T>  storage_type;
+  typedef const T               &return_type;
 };
 
 template<class T>
 struct smoc_storage_out
 {
-  typedef smoc_storage<T>	     storage_type;
-  typedef smoc_write_only_storage<T> return_type;
+  typedef smoc_storage<T>        storage_type;
+  typedef smoc_storage_wom<T>    return_type;
+};
+
+template<class T>
+struct smoc_storage_inout
+{
+  typedef smoc_storage<T>        storage_type;
+  typedef smoc_storage_rw<T>     return_type;
 };
 
 template<>
@@ -138,6 +159,13 @@ struct smoc_storage_in<void>
 
 template<>
 struct smoc_storage_out<void>
+{
+  typedef void storage_type;
+  typedef void return_type;
+};
+
+template<>
+struct smoc_storage_inout<void>
 {
   typedef void storage_type;
   typedef void return_type;
