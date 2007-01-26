372cef96946fd6c2d72efd8eae16944a4e448093
Joachim Falk <falk@cs.fau.de>
2007-01-26 14:51:48 +0000

    * Begun adding systemoc-sdf-clustering examples from clustering techreport
    git-archimport-id: falk@cs.fau.de--2006-systemoc-stuff/SystemC-Examples--mainline--0.5--patch-18

---
+++ systemoc-sdf-clustering/design-cluster-sdf-2.cpp
@@ -0,0 +1,186 @@
+// vim: set sw=2 ts=8:
+/*
+ * Copyright (c) 2004-2006 Hardware-Software-CoDesign, University of
+ * Erlangen-Nuremberg. All rights reserved.
+ * 
+ *   This program is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free Software
+ *   Foundation; either version 2 of the License, or (at your option) any later
+ *   version.
+ * 
+ *   This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU General Public License along with
+ *   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ *   Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
+ * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
+ * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
+ * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
+ * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+ * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
+ * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include <smoc_moc.hpp>
+#include <smoc_port.hpp>
+#include <smoc_fifo.hpp>
+#include <smoc_node_types.hpp>
+#ifndef __SCFE__
+//# include <smoc_scheduler.hpp>
+# include <smoc_pggen.hpp>
+#endif
+
+using namespace std;
+//template <class T>
+class m_adder: public smoc_actor {
+  public:
+    smoc_port_in<int>  in1;
+    smoc_port_in<int>  in2;
+    smoc_port_out<int> out;
+  private:
+    void process() {
+      int retval = in1[0] + in1[1] + in2[0];
+      out[0] = retval;
+#ifndef NDEBUG      
+      cout << name() << " adding " << in1[0] 
+                     << " + " << in1[1] << " + " << in2[0] 
+                     << " = " << retval << std::endl;
+#endif
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_adder( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = (in1(2) && in2(1) && out(1)) >> CALL(m_adder::process) >> start;
+    }
+};
+
+//template <class T>
+class m_multiply: public smoc_actor {
+  public:
+    smoc_port_in<int>  in1;
+    smoc_port_in<int>  in2;
+    smoc_port_out<int> out1;
+    smoc_port_out<int> out2;
+  private:
+    void process() {
+      int retval = in1[0] * in2[0];
+      
+      out1[0] = retval;
+      out2[0] = retval;
+#ifndef NDEBUG
+      cout << name() << " multiplying " << in1[0] 
+                     << " * " << in2[0] << " = " << retval << std::endl;
+#endif
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_multiply( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = (in1(1) && in2(1) && 
+               out1(1) && out2(1)) >>
+               CALL(m_multiply::process) >> start;
+    }
+};
+
+class m_top2
+  : public smoc_graph {
+  public:
+    smoc_port_in<int>  in1;
+    smoc_port_in<int>  in2;
+    smoc_port_out<int> out;
+    
+    m_top2( sc_module_name name )
+      : smoc_graph(name)
+    {
+      m_adder    &adder = registerNode(new m_adder("adder"));
+      m_multiply &mult  = registerNode(new m_multiply("multiply"));
+      
+      connectInterfacePorts( in1, adder.in1 ); // adder.in(in1);
+      connectInterfacePorts( in2, mult.in1 );  // mult.in1(in2);
+      connectNodePorts( adder.out, mult.in2 );
+#ifndef KASCPAR_PARSING
+      connectNodePorts( mult.out2, adder.in2, smoc_fifo<int>() << 13 );
+#endif
+      connectInterfacePorts( out, mult.out1 ); // mult.out(out);
+    }
+};
+
+class m_source: public smoc_actor {
+  public:
+    smoc_port_out<int> out;
+  private:
+    int i;
+    
+    void process() {
+#ifndef NDEBUG
+      cout << name() << " generating " << i << std::endl;
+#endif
+      out[0] = i++;
+    }
+
+    smoc_firing_state start;
+  public:
+    m_source( sc_module_name name )
+      :smoc_actor( name, start ), i(0) {
+      start =  out(1) >> (VAR(i) < 1000000) >> CALL(m_source::process) >> start;
+    }
+};
+
+class m_sink: public smoc_actor {
+  public:
+    smoc_port_in<int> in;
+  private:
+    void process() {
+#ifndef NDEBUG
+      cout << name() << " receiving " << in[0] << std::endl;
+#endif
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_sink( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = in(1) >> CALL(m_sink::process) >> start;
+    }
+};
+
+class m_top
+: public smoc_graph {
+  public:
+    m_top( sc_module_name name )
+      : smoc_graph(name) {
+      m_top2        &top2 = registerNode(new m_top2("top2"));
+      m_source      &src1 = registerNode(new m_source("src1"));
+      m_source      &src2 = registerNode(new m_source("src2"));
+      m_sink        &sink = registerNode(new m_sink("sink"));
+#ifndef KASCPAR_PARSING      
+      connectNodePorts( src1.out, top2.in1, smoc_fifo<int>(2) );
+      connectNodePorts( src2.out, top2.in2, smoc_fifo<int>(2) );
+      connectNodePorts( top2.out, sink.in,  smoc_fifo<int>(2) );
+#endif
+    }
+};
+
+int sc_main (int argc, char **argv) {
+  smoc_top_moc<m_top> top("top");
+  sc_start(-1);
+  return 0;
+}
