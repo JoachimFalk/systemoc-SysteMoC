b2acc42347896d32a56ccf4ab57b02ee579f1fe4
Jens Gladigau <gladigau@codesign.informatik.uni-erlangen.de>
2007-02-20 13:03:17 +0000

    * finished autotest @systemoc-sqr/autotest.sh
    git-archimport-id: falk@cs.fau.de--2007-systemoc-intern/SystemC-Examples--mainline--0.6--patch-21

--- systemoc-sqr/autotest.sh
+++ systemoc-sqr/autotest.sh
@@ -1,11 +1,32 @@
-# -will be processed by BASH, shebang is inserted by automake-
+# -will be processed by BASH, shebang is inserted by make-
 
-# simple autotest for the SQR example. We just start the binary, make sure
-# no output confuses caller, and return exit code
-#./simulation-sqr &> /dev/null
+#
+# simple autotest for the SQR example. We just start the binary with parameter
+# and check the output. Some confusing debug output is send to nirvana.
+#
+# autovars like $srcdir are inserted by make.
+#
 
-# 
-test_bin=$buildir/simulation-sqr
+#
+golden_logs_dir=$srcdir/testlogs
 
-echo "$builddir"
-echo "$srcdir"
+logoutfile=autotest.log
+
+#
+test_bin=$builddir/simulation-sqr
+
+# call binary
+$test_bin 100 > $logoutfile 2> /dev/null
+
+# make sure 'golden' logfile exist! i leave checks for clarity
+diff $logoutfile $golden_logs_dir/100iter.log > /dev/null
+
+if [ $? -gt 0 ]; then
+  echo "SQR autotest: logfiles differ."
+  test_failed=1
+fi
+
+# remove test logfile
+rm -f $logoutfile
+
+[[ -z "$test_failed" ]]
