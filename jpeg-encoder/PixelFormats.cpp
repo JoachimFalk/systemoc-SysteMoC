03b705f76fe5b791974b45da9c36c57d901a24d0
Joachim Falk <falk@cs.fau.de>
2007-03-09 16:48:33 +0000

    * Added clipping to rgb => YCbCr color space transformation
    git-archimport-id: falk@cs.fau.de--2007-systemoc-intern/SystemC-Examples--mainline--0.6--patch-41

--- jpeg-encoder/PixelFormats.cpp
+++ jpeg-encoder/PixelFormats.cpp
@@ -29,8 +29,32 @@ YCbCr_data YCbCr::fromRGB(const RGB_data &x) {
   int r = x.r;
   int g = x.g;
   int b = x.b;
+  
+  assert(r >= 0   && g >= 0   && b >= 0);
+  assert(r <= 255 && g <= 255 && b <= 255);
+  
   int Y = (1225*r + 2404*g + 467*b) >> 8;
   int Cb = ((145*(16*b-Y)) >> 8) + 16*128;
   int Cr = ((183*(16*r-Y)) >> 8) + 16*128;
-  return YCbCr_data(Y>>4,Cb>>4,Cr>>4);
+
+  // round and to 8 bit
+  Y  = (Y  >> 4) + ((Y  >> 3) & 1);
+  Cb = (Cb >> 4) + ((Cb >> 3) & 1);
+  Cr = (Cr >> 4) + ((Cr >> 3) & 1);
+
+  // clip
+  if (Y >= 256)
+    Y  = 255;
+
+  if (Cb >= 256)
+    Cb = 255;
+  else if (Cb < 0)
+    Cb = 0;
+
+  if (Cr >= 256)
+    Cr = 255;
+  else if (Cr < 0)
+    Cr = 0;
+
+  return YCbCr_data(Y, Cb, Cr);
 }
