e6e04976259dcb71276959c7b0bc6312fd0b8ce9
Thomas Schlichter <schlichter@cs.fau.de>
2007-04-05 15:20:47 +0000

    Transform port access patterns to utilize parallel port accesses in actors col2block and block2row
    git-archimport-id: joachim.falk@gmx.de--2005-systemc-stuff/SystemC-Examples--mainline--0.4--patch-80

--- systemoc-idct2d/col2block.hpp
+++ systemoc-idct2d/col2block.hpp
@@ -90,22 +90,16 @@ private:
 
 	
   void action0() {
-    for ( int i = 0; i < 8; i++ )
+    for ( int i = 0; i < 8; i++ ) {
       b[0*8 + i] = R0[i];
-    for ( int i = 0; i < 8; i++ )
       b[1*8 + i] = R1[i];
-    for ( int i = 0; i < 8; i++ )
       b[2*8 + i] = R2[i];
-    for ( int i = 0; i < 8; i++ )
       b[3*8 + i] = R3[i];
-    for ( int i = 0; i < 8; i++ )
       b[4*8 + i] = R4[i];
-    for ( int i = 0; i < 8; i++ )
       b[5*8 + i] = R5[i];
-    for ( int i = 0; i < 8; i++ )
       b[6*8 + i] = R6[i];
-    for ( int i = 0; i < 8; i++ )
       b[7*8 + i] = R7[i];
+    }
 #ifndef NDEBUG
 		print_input();
 #endif
