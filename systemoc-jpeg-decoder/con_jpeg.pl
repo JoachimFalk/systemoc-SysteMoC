a4f67d1dc9ab4af6a0e8c025d5470f468fa44e2b
Thomas Schlichter <schlichter@cs.fau.de>
2007-06-21 19:07:45 +0000

    Extended systemoc-jpeg-decoder/con_jpeg.pl to dump JPEG-Data into a "switch"-statement - This can be used in SystemC-RTL Files to initialize ROMs
    git-archimport-id: falk@cs.fau.de--2007-systemoc-intern/SystemC-Examples--mainline--0.6--patch-267

--- systemoc-jpeg-decoder/con_jpeg.pl
+++ systemoc-jpeg-decoder/con_jpeg.pl
@@ -1,9 +1,71 @@
 #! /usr/bin/perl -w 
 
-use IO::File;
+use warnings;
+use strict;
 
-if ($#ARGV < 0) {
-  print "usage: con_jpeg.pl file1 [file2 [file3 [file4 [...]]]]\n";
+# fix the @INC path
+BEGIN {
+  use File::Spec;
+  use File::Basename;
+  use Cwd qw();
+  
+  @INC = grep { $_ ne '.' } @INC;
+  unshift @INC, File::Spec->catfile(Cwd::abs_path(dirname($0)), "site_perl");
+}
+
+use IO::File;
+use Getopt::Long;
+
+use vars qw($prog);
+
+# Get the program name
+$prog = basename($0);
+
+
+sub Usage {
+  print $#ARGV."\n";
+  my $error = (@_);
+
+  print STDERR "\n".
+  print STDERR "usage: $prog [-h|-?|--help] [-s|--switch] file1 [file2 [file3 [file4 [...]]]]\n";
+  exit($error ? 1 : 0);
+}
+
+my $opt = {};
+
+sub print_value {
+  my $addr = shift;
+  my $value = shift;
+
+  if($opt->{'switch_output'}){
+    print "    case $addr: temp_val = $value; break;\n";
+  }else{
+    print "$value, \n";
+  }
+
+}
+
+my $p = new Getopt::Long::Parser;
+$p->configure("gnu_getopt");
+my $rc = $p->getoptions(
+  'help|h|?'                => \$opt->{'help'},
+  'switch|s'           => \$opt->{'switch_output'},
+);
+
+if (!$rc || $opt->{'help'} || 
+    ($#ARGV <0)) {
+  &Usage(!$opt->{'help'});
+  
+}
+
+
+my $addr = 0;
+
+
+if($opt->{'switch_output'}){
+  print "short picture_memory(unsigned int addr){\n";
+  print "  short temp_val = 0;\n";
+  print "  switch(addr) {\n";
 }
 
 
@@ -13,7 +75,8 @@ for(my $i = 0; $i <= $#ARGV; $i++){
   if (!$fh->eof){
     # File is not empty
     # Insert start of file-marker
-    print "-1, \n";
+    print_value($addr,-1);
+    $addr++;
   }
 
   while (!$fh->eof) {
@@ -21,8 +84,20 @@ for(my $i = 0; $i <= $#ARGV; $i++){
 
     $fh->read($byte, 1, 0);
 
-    print ord($byte), ", \n";
+    print_value($addr,ord($byte));
+    $addr++;
   }
 
 
+}
+
+
+
+if($opt->{'switch_output'}){
+  print "    default: temp_val = 0; break;\n";
+
+  print "  }\n";
+#  print "  block_data_size = $addr;\n";
+  print "  return temp_val;\n";
+  print "}\n";
 }
