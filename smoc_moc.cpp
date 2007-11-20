075be240708edad660b2218e8aabdd2a4d0bf4cc
Martin Streubuehr <streubuehr@cs.fau.de>
2007-11-20 15:09:52 +0000

    - fixed spooky bug with tick in SR
    git-archimport-id: falk@cs.fau.de--2007-systemoc-intern/SysteMoC--mainline--0.6--patch-150

--- smoc_moc.cpp
+++ smoc_moc.cpp
@@ -356,8 +356,7 @@ void smoc_scheduler_top::scheduleSR(smoc_graph *c) {
 
     // move transition to next list
 #ifdef SYSTEMOC_ENABLE_VPC
-    //FIXME
-    //assert(n.inCommState());
+    assert(n.inCommState());
     for ( transitionlist_ty::iterator titer = n._currentState->tl.begin();
     titer != n._currentState->tl.end();
     ++titer ){
