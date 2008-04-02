8ac063ab7e838ca25b886470d8ac9b009c88c119
Christian Haubelt <haubelt@cs.fau.de>
2008-04-02 07:31:38 +0000

    Added triplex voters
    git-archimport-id: falk@cs.fau.de--2007-systemoc-intern/SystemC-Examples--mainline--0.6--patch-344

---
+++ systemoc-voter/TriplexVoter_IDCTCoeff_t.cpp
@@ -0,0 +1,227 @@
+// vim: set sw=2 ts=8:
+
+#include "channels.hpp"
+#include "TriplexVoter_IDCTCoeff_t.hpp"
+
+
+void TriplexVoter::forward_in1() {
+    out[0] = in1[0];
+    outTimeOut[0] = true;
+}
+
+void TriplexVoter::forward_in2() {
+    out[0] = in2[0];
+    outTimeOut[0] = true;
+}
+
+void TriplexVoter::read_in1() {
+  int i = in1[0];
+}
+
+void TriplexVoter::read_in2() {
+  int i = in2[0];
+}
+
+void TriplexVoter::read_in3() {
+  int i = in3[0];
+}
+
+void TriplexVoter::failed_action() {
+  int i = inTimeOut[0];
+}
+
+TriplexVoter::TriplexVoter( sc_module_name name )
+     : smoc_actor(name,start)
+  {
+    start = (in1(1) && in2(1) && in3(1) && (in1.getValueAt(0) == in2.getValueAt(0)) 
+              && (in1.getValueAt(0) == in3.getValueAt(0))) 
+         >> (out(1) && outTimeOut(1)) 
+         >> CALL(TriplexVoter::forward_in1) 
+         >> start
+       | (in1(1) && in2(1) && in3(1) && (in1.getValueAt(0) == in2.getValueAt(0)) 
+              && (in1.getValueAt(0) != in3.getValueAt(0)))
+         >> (out(1) && outTimeOut(1)) 
+         >> CALL(TriplexVoter::forward_in1) 
+         >> failed_in3
+       | (in1(1) && in2(1) && in3(1) && (in1.getValueAt(0) != in2.getValueAt(0)) 
+              && (in1.getValueAt(0) == in3.getValueAt(0)))
+         >> (out(1) && outTimeOut(1)) 
+         >> CALL(TriplexVoter::forward_in1) 
+         >> failed_in2
+       | (in1(1) && in2(1) && in3(1) && (in1.getValueAt(0) != in2.getValueAt(0)) 
+              && (in2.getValueAt(0) == in3.getValueAt(0)))
+         >> (out(1) && outTimeOut(1)) 
+         >> CALL(TriplexVoter::forward_in2) 
+         >> failed_in1
+       | (in1(1) && in2(1) && in3(1) && (in1.getValueAt(0) != in2.getValueAt(0)) 
+              && (in2.getValueAt(0) != in3.getValueAt(0)))
+         >> failed
+       | inTimeOut(1) >> CALL(TriplexVoter::failed_action)
+         >> timeOut;
+    timeOut = (in1(0, 1)) >> CALL(TriplexVoter::read_in1)
+           >> failed_in2_or_in3
+         | (in2(0, 1)) >> CALL(TriplexVoter::read_in2)
+           >> failed_in1_or_in3
+         | (in3(0, 1)) >> CALL(TriplexVoter::read_in3)
+           >> failed_in1_or_in2;
+    failed_in2_or_in3 = (in1(1) && in2(1) && (in1.getValueAt(0) == in2.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in1) 
+                     >> failed_in3
+                   | (in1(1) && in2(1) && (in1.getValueAt(0) != in2.getValueAt(0)))
+                     >> failed
+                   | (in1(1) && in3(1) && (in1.getValueAt(0) == in3.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in1) 
+                     >> failed_in2
+                   | (in1(1) && in3(1) && (in1.getValueAt(0) != in3.getValueAt(0)))
+                     >> failed;
+    failed_in1_or_in3 = (in1(1) && in2(1) && (in1.getValueAt(0) == in2.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in1) 
+                     >> failed_in3
+                   | (in1(1) && in2(1) && (in1.getValueAt(0) != in2.getValueAt(0)))
+                     >> failed
+                   | (in2(1) && in3(1) && (in2.getValueAt(0) == in3.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in2) 
+                     >> failed_in1
+                   | (in2(1) && in3(1) && (in2.getValueAt(0) != in3.getValueAt(0)))
+                     >> failed;
+    failed_in1_or_in2 = (in1(1) && in3(1) && (in1.getValueAt(0) == in3.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in1) 
+                     >> failed_in2
+                   | (in1(1) && in3(1) && (in1.getValueAt(0) != in3.getValueAt(0)))
+                     >> failed
+                   | (in2(1) && in3(1) && (in2.getValueAt(0) == in3.getValueAt(0)))
+                     >> (out(1) && outTimeOut(1))
+                     >> CALL(TriplexVoter::forward_in2) 
+                     >> failed_in1
+                   | (in2(1) && in3(1) && (in2.getValueAt(0) != in3.getValueAt(0)))
+                     >> failed;
+    failed_in1 = (in2(1) && in3(1) && (in2.getValueAt(0) == in3.getValueAt(0)))
+               >> (out(1) && outTimeOut(1))
+               >> CALL(TriplexVoter::forward_in2) 
+               >> failed_in1
+             | (in2(1) && in3(1) && (in2.getValueAt(0) != in3.getValueAt(0)))
+               >> failed
+             | inTimeOut(1) >> CALL(TriplexVoter::failed_action)
+               >> failed;
+     failed_in2 = (in1(1) && in3(1) && (in1.getValueAt(0) == in3.getValueAt(0)))
+               >> (out(1) && outTimeOut(1))
+               >> CALL(TriplexVoter::forward_in1) 
+               >> failed_in2
+             | (in1(1) && in3(1) && (in1.getValueAt(0) != in3.getValueAt(0)))
+               >> failed
+             | inTimeOut(1) >> CALL(TriplexVoter::failed_action)
+               >> failed;
+     failed_in3 = (in1(1) && in2(1) && (in1.getValueAt(0) == in2.getValueAt(0)))
+               >> (out(1) && outTimeOut(1))
+               >> CALL(TriplexVoter::forward_in1) 
+               >> failed_in3
+             | (in1(1) && in2(1) && (in1.getValueAt(0) != in2.getValueAt(0)))
+               >> failed
+             | inTimeOut(1) >> CALL(TriplexVoter::failed_action)
+               >> failed;
+
+  }
+
+class m_source_IDCTCoeff_t: public smoc_actor {
+  public:
+    smoc_port_out<IDCTCoeff_t> out;
+  private:
+    
+    void process() {
+      out[0] = 0;
+    }
+
+    smoc_firing_state start;
+  public:
+    m_source_IDCTCoeff_t( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start =  out(1) >> CALL(m_source_IDCTCoeff_t::process) >> start;
+    }
+};
+
+class m_sink_IDCTCoeff_t: public smoc_actor {
+  public:
+    smoc_port_in<IDCTCoeff_t> in;
+  private:
+    void process() {
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_sink_IDCTCoeff_t( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = in(1) >> CALL(m_sink_IDCTCoeff_t::process) >> start;
+    }
+};
+
+class m_source_bool: public smoc_actor {
+  public:
+    smoc_port_out<int> out;
+  private:
+    void process() {
+      out[0] = 1;
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_source_bool( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = out(0,1) >> CALL(m_source_bool::process) >> start;
+    }
+};
+
+class m_sink_bool: public smoc_actor {
+  public:
+    smoc_port_in<int> in;
+  private:
+    void process() {
+    }
+    
+    smoc_firing_state start;
+  public:
+    m_sink_bool( sc_module_name name )
+      :smoc_actor( name, start ) {
+      start = in(1) >> CALL(m_sink_bool::process) >> start;
+    }
+};
+
+
+class TriplexTester: public smoc_graph {
+
+public:
+  TriplexTester(sc_module_name name) : smoc_graph(name), voter("Voter"), src_bool("Source_Bool"), src_1("Source_1"), src_2("Source_2"), src_3("Source_3"), sink_bool("Sink_Bool"), sink("Sink") {
+    connectNodePorts<2>(src_bool.out, voter.inTimeOut);
+    connectNodePorts<2>(src_1.out, voter.in1);
+    connectNodePorts<2>(src_2.out, voter.in2);
+    connectNodePorts<2>(src_3.out, voter.in3);
+    connectNodePorts<2>(voter.outTimeOut, sink_bool.in);
+    connectNodePorts<2>(voter.out, sink.in);
+  }
+  
+private:
+
+  m_source_bool src_bool;
+  m_source_IDCTCoeff_t src_1;
+  m_source_IDCTCoeff_t src_2;
+  m_source_IDCTCoeff_t src_3;
+  m_sink_bool sink_bool;
+  m_sink_IDCTCoeff_t sink;
+  TriplexVoter voter;
+
+
+};
+
+int sc_main (int argc, char **argv) {
+    
+  smoc_top_moc<TriplexTester> tester("Tester");
+  
+  sc_start();
+  
+  return 0;
+}
+
---
+++ systemoc-voter/TriplexVoter_Pixel_t.hpp
@@ -0,0 +1,52 @@
+// vim: set sw=2 ts=8:
+
+#ifndef INCLUDE_TRIPLEX_VOTER_HPP
+#define INCLUDE_TRIPLEX_VOTER_HPP
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include <systemoc/smoc_moc.hpp>
+#include <systemoc/smoc_port.hpp>
+#include <systemoc/smoc_node_types.hpp>
+#ifndef __SCFE__
+# include <systemoc/smoc_pggen.hpp>
+#endif
+
+#include "channels.hpp"
+
+///Duplication of a token
+class TriplexVoter: public smoc_actor {
+
+public:
+  smoc_port_in<Pixel_t> in1;
+  smoc_port_in<Pixel_t> in2;
+  smoc_port_in<Pixel_t> in3;
+  smoc_port_in<int> inTimeOut;
+  smoc_port_out<Pixel_t> out;
+  smoc_port_out<int> outTimeOut;
+
+  TriplexVoter( sc_module_name name );
+
+private:
+  void forward_in1(); 
+  void forward_in2(); 
+  void read_in1(); 
+  void read_in2(); 
+  void read_in3(); 
+  void failed_action();
+
+  smoc_firing_state start;
+  smoc_firing_state timeOut;
+  smoc_firing_state failed_in1;
+  smoc_firing_state failed_in2;
+  smoc_firing_state failed_in3;
+  smoc_firing_state failed_in1_or_in2;
+  smoc_firing_state failed_in1_or_in3;
+  smoc_firing_state failed_in2_or_in3;
+  smoc_firing_state failed;
+  
+};
+
+#endif // INCLUDE_TRIPLEX_VOTER_HPP
