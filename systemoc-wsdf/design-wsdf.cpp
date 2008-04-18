4aee6879f871c88727cabdd0213b4c0c194fdcef
Joachim Keinert <keinert@codesign.informatik.uni-erlangen.de>
2008-04-18 15:49:11 +0000

    - Merge patch-352 from mainline--0.6 - Fix indent in systemoc-wsdf/design-wsdf.cpp
    Patches applied:
    
     * falk@cs.fau.de--2007-systemoc-intern/SystemC-Examples--mainline--0.6--patch-352
       - Fix indent in systemoc-wsdf/design-wsdf.cpp
    
    
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/SystemC-Examples--mainline--0.7--patch-1

--- systemoc-wsdf/design-wsdf.cpp
+++ systemoc-wsdf/design-wsdf.cpp
@@ -1,3 +1,4 @@
+//  -*- tab-width:8; intent-tabs-mode:nil;  c-basic-offset:2; -*-
 // vim: set sw=2 ts=8:
 
 #include <cstdlib>
@@ -19,151 +20,151 @@ using namespace ns_smoc_vector_init;
 
 class m_source: public smoc_actor {
 public:
-	smoc_md_port_out<int,2> out;
+  smoc_md_port_out<int,2> out;
 private:
-	int i;
+  int i;
   
-	void process() {
+  void process() {
 #ifndef NDEBUG
-		cout << name() << " generating " << i << std::endl;
+    cout << name() << " generating " << i << std::endl;
 #endif
-		out[0][0] = i++;
-	}
+    out[0][0] = i++;
+  }
 	
-	smoc_firing_state start;
+  smoc_firing_state start;
 public:
-	m_source( sc_module_name name )
-		:smoc_actor( name, start ), 
-		 out(ul_vector_init[1][1] <<
-				 ul_vector_init[12][9]),
-		 i(0)		 
-	{
-		start =  out(1) >> (VAR(i) < 12*9+24) >> CALL(m_source::process) >> start;
-	}
+  m_source( sc_module_name name )
+    :smoc_actor( name, start ), 
+     out(ul_vector_init[1][1] <<
+         ul_vector_init[12][9]),
+     i(0)		 
+  {
+    start =  out(1) >> (VAR(i) < 12*9+24) >> CALL(m_source::process) >> start;
+  }
 };
 
 class m_sink: public smoc_actor {
 public:
-	smoc_md_port_in<int,2> in;
+  smoc_md_port_in<int,2> in;
 
 private:
 
-	ifstream ref_data_file;
+  ifstream ref_data_file;
 
-	void process() {
+  void process() {
 #ifndef NDEBUG
-		cout << name() << " receiving " << in[0][0] << std::endl;
+    cout << name() << " receiving " << in[0][0] << std::endl;
 
 
-		double ref_pixel;
-		ref_data_file >> ref_pixel;		
-		if (!ref_data_file.eof()){
-			cout << name() << " reference value " << ref_pixel << std::endl;
-			assert((int)ref_pixel == in[0][0]);
-		}else{
-			cout << name() << " no reference value" << std::endl;
-		}
+    double ref_pixel;
+    ref_data_file >> ref_pixel;		
+    if (!ref_data_file.eof()){
+      cout << name() << " reference value " << ref_pixel << std::endl;
+      assert((int)ref_pixel == in[0][0]);
+    }else{
+      cout << name() << " no reference value" << std::endl;
+    }
 #endif
 		
-	}
+  }
   
-	smoc_firing_state start;
+  smoc_firing_state start;
 public:
-	m_sink( sc_module_name name )
-		:smoc_actor( name, start ),
-		 in(ul_vector_init[12][9], //firing_blocks
-				ul_vector_init[12][9], //u0
-				ul_vector_init[1][1], //c
-				ul_vector_init[1][1], //delta_c
-				sl_vector_init[0][0], //bs
-				sl_vector_init[0][0] //bt
-				),				
-		 ref_data_file(REF_FILENAME)
-	{
-		start = in(1) >> CALL(m_sink::process) >> start;
-	}
+  m_sink( sc_module_name name )
+    :smoc_actor( name, start ),
+     in(ul_vector_init[12][9], //firing_blocks
+        ul_vector_init[12][9], //u0
+        ul_vector_init[1][1], //c
+        ul_vector_init[1][1], //delta_c
+        sl_vector_init[0][0], //bs
+        sl_vector_init[0][0] //bt
+        ),				
+     ref_data_file(REF_FILENAME)
+  {
+    start = in(1) >> CALL(m_sink::process) >> start;
+  }
 };
 
 class m_filter: public smoc_actor {
 public:
-	smoc_md_port_in<int,2> in;
-	smoc_md_port_out<int,2> out;
+  smoc_md_port_in<int,2> in;
+  smoc_md_port_out<int,2> out;
 private:
-	void process() {
-		const int filter_size = 3;
+  void process() {
+    const int filter_size = 3;
 #ifndef NDEBUG
-		cout << "=======================================" << std::endl;
-		cout << name() << std::endl;
-		cout << "Window pixels:" << std::endl;
-		for(unsigned y = 0; y < 3; y++){
-			for(unsigned x = 0; x < 1; x++){
-				cout << in[x][y] << " ";
-			}
-			cout << std::endl;
-		}
+    cout << "=======================================" << std::endl;
+    cout << name() << std::endl;
+    cout << "Window pixels:" << std::endl;
+    for(unsigned y = 0; y < 3; y++){
+      for(unsigned x = 0; x < 1; x++){
+        cout << in[x][y] << " ";
+      }
+      cout << std::endl;
+    }
 #endif
-		int output_value = 0;
-		for(unsigned y = 0; y < 3; y++){
-			for(unsigned x = 0; x < 1; x++){
-				output_value += in[x][y];
-			}
-		}
-		output_value /= filter_size;
+    int output_value = 0;
+    for(unsigned y = 0; y < 3; y++){
+      for(unsigned x = 0; x < 1; x++){
+        output_value += in[x][y];
+      }
+    }
+    output_value /= filter_size;
 #ifndef NDEBUG
-		cout << "Output value: " << output_value << std::endl;
+    cout << "Output value: " << output_value << std::endl;
 #endif
-		out[0][0] = output_value;		
-	}
+    out[0][0] = output_value;		
+  }
   
-	smoc_firing_state start;
+  smoc_firing_state start;
 public:
-	m_filter( sc_module_name name )
-		: smoc_actor( name, start ) ,
-			in(ul_vector_init[12][9], //firing_blocks
-				 ul_vector_init[12][9], //u0
-				 ul_vector_init[1][3], //c
-				 ul_vector_init[1][1], //delta_c
-				 sl_vector_init[0][1], //bs
-				 sl_vector_init[0][1] //bt
-				 ),
-			out(ul_vector_init[1][1] <<
-					ul_vector_init[12][9])
+  m_filter( sc_module_name name )
+    : smoc_actor( name, start ) ,
+      in(ul_vector_init[12][9], //firing_blocks
+         ul_vector_init[12][9], //u0
+         ul_vector_init[1][3], //c
+         ul_vector_init[1][1], //delta_c
+         sl_vector_init[0][1], //bs
+         sl_vector_init[0][1] //bt
+         ),
+      out(ul_vector_init[1][1] <<
+          ul_vector_init[12][9])
 		
-	{
-		start = (in(1) && out(1)) >> CALL(m_filter::process) >> start;
-	}
+  {
+    start = (in(1) && out(1)) >> CALL(m_filter::process) >> start;
+  }
 };
 
 class m_top2
   : public smoc_graph {
 public:
-	smoc_md_iport_in<int,2>  in;
-	smoc_md_iport_out<int,2> out;
+  smoc_md_iport_in<int,2>  in;
+  smoc_md_iport_out<int,2> out;
     
-	m_top2( sc_module_name name )
-		: smoc_graph(name)
-	{
-		m_filter      &filter = registerNode(new m_filter("filter"));
+  m_top2( sc_module_name name )
+    : smoc_graph(name)
+  {
+    m_filter      &filter = registerNode(new m_filter("filter"));
       
-		filter.in(in);
-		filter.out(out);
-	}
+    filter.in(in);
+    filter.out(out);
+  }
 };
 
 class m_top
-	: public smoc_graph {
+  : public smoc_graph {
 public:
-	m_top( sc_module_name name )
-		: smoc_graph(name) {
-		m_source      &src = registerNode(new m_source("src"));
-		m_top2        &top2 = registerNode(new m_top2("m_top2"));
-		m_sink        &sink = registerNode(new m_sink("sink"));
+  m_top( sc_module_name name )
+    : smoc_graph(name) {
+    m_source      &src = registerNode(new m_source("src"));
+    m_top2        &top2 = registerNode(new m_top2("m_top2"));
+    m_sink        &sink = registerNode(new m_sink("sink"));
 
 #ifndef KASCPAR_PARSING
-		indConnectNodePorts( src.out, top2.in, smoc_wsdf_edge<int>(3));
-		indConnectNodePorts( top2.out, sink.in,smoc_wsdf_edge<int>(1));
+    indConnectNodePorts( src.out, top2.in, smoc_wsdf_edge<int>(3));
+    indConnectNodePorts( top2.out, sink.in,smoc_wsdf_edge<int>(1));
 #endif
-	}
+  }
 };
 
 int sc_main (int argc, char **argv) {
