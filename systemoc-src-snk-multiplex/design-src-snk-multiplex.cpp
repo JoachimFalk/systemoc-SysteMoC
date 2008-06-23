f4bb5541449291a30d9c8b45fc9b5920a88bdbf5
Christian Zebelein <christian.zebelein@cs.fau.de>
2008-06-23 12:17:13 +0000

    * randomized systemoc-src-snk-multiplex
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/SysteMoC-Testcases--mainline--0.1--patch-40

--- systemoc-src-snk-multiplex/design-src-snk-multiplex.cpp
+++ systemoc-src-snk-multiplex/design-src-snk-multiplex.cpp
@@ -35,6 +35,9 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <ctime>
+
+#include <sys/time.h>
 
 #include <systemoc/smoc_moc.hpp>
 #include <systemoc/smoc_port.hpp>
@@ -44,6 +47,23 @@
 //# include <smoc_scheduler.hpp>
 # include <systemoc/smoc_pggen.hpp>
 #endif
+
+#include <CoSupport/DataTypes/CheckedVector.hpp>
+#include <CoSupport/String/Concat.hpp>
+
+using CoSupport::DataTypes::CheckedVector;
+using CoSupport::String::Concat;
+
+// random number in range [l,h[
+size_t rand(size_t l, size_t h) {
+  return l + (size_t) ((double)h * (std::rand() / (RAND_MAX + 1.0)));
+}
+
+uint64_t getUSecs() {
+  timeval tv;
+  gettimeofday(&tv,0);
+  return tv.tv_sec * 1000000ull + tv.tv_usec;
+}
 
 typedef std::pair<size_t, int> Token;
 
@@ -60,73 +80,121 @@ class m_h_src: public smoc_actor {
 public:
   smoc_port_out<Token> out;
 private:
-  int     i;
-  size_t  iter;
-  
+  size_t iter, inst;
+  int val;
+
   void src() {
+    Token t(rand(0, inst), val++);
+
     std::cout
-      << name() << ": generate token with id "
-      << out.tokenId(0) << " with value " << i << std::endl;
-    out[0] = std::make_pair(-1, i++); --iter;
+      << name() << ": generate token with id " << out.tokenId(0)
+      << " and value " << t << std::endl;
+  
+    out[0] = t;
+
+    assert(iter > 0u); 
+    --iter;
   }
+
+  bool more() const
+    { return iter > 0u; }
+
   smoc_firing_state start;
 public:
-  m_h_src(sc_module_name name, size_t _iter, size_t _start)
+  m_h_src(sc_module_name name, size_t iter, size_t inst)
     : smoc_actor(name, start),
-      i(_start), iter(_iter) {
+      iter(iter),
+      inst(inst),
+      val(0)
+  {
     start =
-         (out(1) && VAR(iter) > 0U)
+         (out(1) && GUARD(m_h_src::more) /*FIXME: VAR(iter) > 0U*/)
       >> CALL(m_h_src::src)       >> start;
   }
 };
 
-class m_h_sink: public smoc_actor {
+class m_h_snk: public smoc_actor {
 public:
   smoc_port_in<Token> in;
 private:
-  void sink(void) {
+  const size_t idx;
+
+  void snk() {
     std::cout
-      << name() << ": received token with id "
-      << in.tokenId(0) << " and value " << in[0] << std::endl;
+      << name() << ": received token with id " << in.tokenId(0)
+      << " and value " << in[0] << std::endl;
+    assert(in[0].first == idx);
   }
   
   smoc_firing_state start;
 public:
-  m_h_sink(sc_module_name name)
-    : smoc_actor(name, start) {
-    start = in(1) >> CALL(m_h_sink::sink) >> start;
+  m_h_snk(sc_module_name name, size_t idx)
+    : smoc_actor(name, start),
+      idx(idx)
+  {
+    start = in(1) >> CALL(m_h_snk::snk) >> start;
   }
+  
+  struct factory {
+  public:
+    factory(const char* p)
+      : p(p)
+      {}
+    void construct(m_h_snk* m, size_t i)
+      { new (m) m_h_snk(Concat(p)(i).get().c_str(), i); }
+  private:
+    const char* p;
+  };
 };
 
 class m_h_top: public smoc_graph {
 protected:
-  m_h_src     src1;
-  m_h_sink    snk1;
-  m_h_src     src2;
-  m_h_sink    snk2;
-  m_h_src     src3;
-  m_h_sink    snk3;
+  m_h_src  src;
+  CheckedVector<m_h_snk> snk;
 public:
-  m_h_top(sc_module_name name, size_t iter)
+  m_h_top(sc_module_name name, size_t iter, size_t inst, size_t size, size_t ooo)
     : smoc_graph(name),
-      src1("src1", iter, 1000), snk1("snk1"),
-      src2("src2", iter, 2000), snk2("snk2"),
-      src3("src3", iter, 3000), snk3("snk3") {
-    // multiplex fifo of depth 17 and 3 out of order
-    smoc_multiplex_fifo<Token, ColorAccessor> f(17,3);
-    connectNodePorts(src1.out, snk1.in, f.getVirtFifo());
-    connectNodePorts(src2.out, snk2.in, f.getVirtFifo());
-    connectNodePorts(src3.out, snk3.in, f.getVirtFifo());
+      src("src", iter, inst),
+      snk(inst, m_h_snk::factory("snk"))
+  {
+    smoc_multiplex_fifo<Token, ColorAccessor> f(size,ooo);
+    f.connect(src.out);
+    for(size_t i = 0; i < inst; ++i) {
+      f.getVirtFifo().connect(snk[i].in);
+    }
   }
 };
 
 int sc_main (int argc, char **argv) {
   size_t iter = static_cast<size_t>(-1);
-  
+  size_t inst = 3;
+  size_t size = 17;
+  size_t ooo = 3;
+  size_t seed = getUSecs();
+ 
+  // number of iterations 
   if (argc >= 2)
     iter = atol(argv[1]);
   
-  smoc_top_moc<m_h_top> top("top", iter);
+  // number of port pairs
+  if (argc >= 3)
+    inst = atol(argv[2]);
+  
+  // size of multiplex fifo 
+  if (argc >= 4)
+    size = atol(argv[3]);
+
+  // size of ooo area
+  if (argc >= 5)
+    ooo = atol(argv[4]);
+  
+  // seed
+  if (argc >= 6)
+    seed =  atol(argv[5]);
+
+  std::srand(seed);
+
+  smoc_top_moc<m_h_top> top("top", iter, inst, size, ooo);
   sc_start();
   return 0;
 }
