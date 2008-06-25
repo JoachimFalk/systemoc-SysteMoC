e9068e40a8dc59aaffed6b803f6cf2ac9d8a8794
Joachim Falk <falk@cs.fau.de>
2008-06-25 19:46:40 +0000

    * gcc 4.1.2 compile fixes
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/SysteMoC--mainline--0.8--patch-130

--- pkginclude/systemoc/smoc_root_node.hpp
+++ pkginclude/systemoc/smoc_root_node.hpp
@@ -158,7 +158,7 @@ public:
   
   const smoc_sysc_port_list getPorts() const;
   
-  const smoc_firing_types::statelist_ty smoc_root_node::getFiringStates() const;
+  const smoc_firing_types::statelist_ty getFiringStates() const;
   
   std::ostream &dumpActor( std::ostream &o );
     
