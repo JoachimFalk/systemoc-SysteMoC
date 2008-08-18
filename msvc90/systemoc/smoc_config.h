62f2025d5e3e9b3bb6aa297312a75a5430441efc
Thomas Schlichter <schlichter@cs.fau.de>
2008-08-18 17:34:41 +0000

    Add project files for Microsoft Visual C++ 2008
    git-archimport-id: falk@cs.fau.de--2008-systemoc-intern/SysteMoC--mainline--0.8--patch-153

---
+++ msvc90/systemoc/smoc_config.h
@@ -0,0 +1,12 @@
+/* vim: set sw=2 ts=8 syn=c: */
+
+#ifndef _INCLUDED_SMOC_CONFIG_H
+#define _INCLUDED_SMOC_CONFIG_H
+
+/* Define if you want SystemC-VPC Support */
+#undef SYSTEMOC_ENABLE_VPC
+
+/* Define if you want WSDF Support */
+#undef SYSTEMOC_ENABLE_WSDF
+
+#endif /* _INCLUDED_SMOC_CONFIG_H */
