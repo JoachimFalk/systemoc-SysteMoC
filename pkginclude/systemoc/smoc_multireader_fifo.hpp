ec9727a2daa5af77975d16d0bc7eb1d47fdd104f
Joachim Falk <falk@cs.fau.de>
2012-09-14 16:50:33 +0200

    Split moc_latency_queues.cpp/hpp

--- pkginclude/systemoc/smoc_multireader_fifo.hpp
+++ pkginclude/systemoc/smoc_multireader_fifo.hpp
@@ -57,11 +57,12 @@
 #include "detail/smoc_storage.hpp"
 #include "smoc_chan_adapter.hpp"
 #include "smoc_fifo.hpp"
-#include "detail/smoc_latency_queues.hpp"
 #include "detail/smoc_fifo_storage.hpp"
 #include <smoc/detail/ConnectProvider.hpp>
 #include <smoc/detail/EventMapManager.hpp>
 #ifdef SYSTEMOC_ENABLE_VPC
+# include <smoc/detail/LatencyQueue.hpp>
+# include <smoc/detail/DIIQueue.hpp>
 # include <smoc/detail/QueueFRVWPtr.hpp>
 #else
 # include <smoc/detail/QueueRWPtr.hpp>
