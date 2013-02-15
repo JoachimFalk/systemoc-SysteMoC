e4be242388e2b9100178169e16fa71b897236308
Nils Ballmann <nils.ballmann@e-technik.stud.uni-erlangen.de>
2013-02-15 17:31:02 +0100

    Actor Demux added

---
+++ pkginclude/actorlibrary/simulink/Demux.hpp
@@ -0,0 +1,42 @@
+/*  Library : Math Operations
+ Block : Product, Divide
+ Despcription : Multiply and divide scalars and nonscalars or multiply and
+ invert matrices.
+ */
+#ifndef __INCLUDED__DEMUX__HPP__
+#define __INCLUDED__DEMUX__HPP__
+
+#include <systemoc/smoc_expr.hpp>
+
+template<typename DATA_TYPE, int PORTS = 1>
+class Demux: public smoc_actor {
+public:
+
+	smoc_port_in<DATA_TYPE> in;
+	smoc_port_out<DATA_TYPE> out[PORTS];
+
+	Demux(sc_module_name name) :
+			smoc_actor(name, start) {
+
+		Expr::Ex<bool>::type eOut(out[0](1));
+
+		for (int i = 1; i < PORTS; i++) {
+			eOut = eOut && out[i](1);
+		}
+
+		start = in(1) >> eOut >> CALL(Demux::process) >> start;
+	}
+protected:
+
+	void process() {
+		for (int i = 0; i < PORTS; i++) {
+			DATA_TYPE tmp = in[0];
+			out[i][0] = tmp;
+		}
+	}
+
+	smoc_firing_state start;
+
+};
+
+#endif// __INCLUDED__DEMUX__HPP__
