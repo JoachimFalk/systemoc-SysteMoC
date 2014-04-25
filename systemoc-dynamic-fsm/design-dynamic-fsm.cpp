c1f82caddf80f5f77a867dbb35dd71e3feb48cf6
Joachim Falk <falk@cs.fau.de>
2014-04-25 16:42:38 +0200

    Added systemoc-dynamic-fsm to demonstrate dynamic creation of actor FSMs

---
+++ systemoc-dynamic-fsm/design-dynamic-fsm.cpp
@@ -0,0 +1,122 @@
+// vim: set sw=2 ts=8:
+/*
+ * Copyright (c) 2004-2006 Hardware-Software-CoDesign, University of
+ * Erlangen-Nuremberg. All rights reserved.
+ * 
+ *   This program is free software; you can redistribute it and/or modify it under
+ *   the terms of the GNU General Public License as published by the Free Software
+ *   Foundation; either version 2 of the License, or (at your option) any later
+ *   version.
+ * 
+ *   This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *   details.
+ * 
+ *   You should have received a copy of the GNU General Public License along with
+ *   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ *   Place, Suite 330, Boston, MA 02111-1307 USA.
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
+ * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
+ * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
+ * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+ * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
+ * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
+ * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+ * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
+ * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ * ENHANCEMENTS, OR MODIFICATIONS.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+#include <systemoc/smoc_moc.hpp>
+#include <systemoc/smoc_port.hpp>
+#include <systemoc/smoc_fifo.hpp>
+#include <systemoc/smoc_node_types.hpp>
+
+class m_h_src: public smoc_actor {
+public:
+  smoc_port_out<int> out;
+private:
+  int i;
+  int iter;
+  
+  void src(int j) {
+    std::cout
+      << name() << ": generate " << j << " token with value " << i << std::endl;
+    for (int x = 0; x < j; ++x) {
+      out[x] = i++;
+    }
+    iter -= j;
+  }
+  smoc_firing_state start;
+public:
+  m_h_src(sc_module_name name, int iter)
+    : smoc_actor(name, start),
+      out("out"),
+      i(1), iter(iter),
+      start("start")
+  {
+    smoc_firing_state::Ptr oldState = &start;
+
+    int mumStates = 3;
+    for (int j = 1; j <= mumStates; ++j) {
+      smoc_firing_state::Ptr newState = j < mumStates
+        ? &smoc_firing_state("state"+CoSupport::String::asStr(j))
+        : &start;
+      *oldState = (out(j) && VAR(this->iter) > 0)
+        >> CALL(m_h_src::src)(j) >> *newState;
+      oldState = newState;
+    }
+  }
+};
+
+
+class m_h_sink: public smoc_actor {
+public:
+  smoc_port_in<int> in;
+private:
+  int i;
+  
+  void sink(void) {
+    std::cout
+      << name() << ": received token with value " << in[0] << std::endl;
+  }
+  
+  smoc_firing_state start;
+public:
+  m_h_sink(sc_module_name name)
+    : smoc_actor(name, start) {
+    start = in(1) >> CALL(m_h_sink::sink) >> start;
+  }
+};
+
+class m_h_top: public smoc_graph {
+protected:
+  m_h_src     src;
+  m_h_sink    snk;
+public:
+  m_h_top(sc_module_name name, size_t iter)
+    : smoc_graph(name),
+      src("src", iter), snk("snk") {
+    connectNodePorts<16>(src.out, snk.in);
+  }
+};
+
+int sc_main (int argc, char **argv) {
+  int iter = std::numeric_limits<int>::max();
+  
+  if (argc >= 2)
+    iter = atol(argv[1]);
+  
+  smoc_top_moc<m_h_top> top("top", iter);
+  sc_start();
+  return 0;
+}
