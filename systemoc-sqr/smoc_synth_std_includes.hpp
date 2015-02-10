63e432854d425fe27dca15b59cf899c26ca307b3
Joachim Falk <falk@cs.fau.de>
2015-02-10 10:38:26 +0100

    Tweaked some stuff for better profiling of generated synth-sw target

--- systemoc-sqr/smoc_synth_std_includes.hpp
+++ systemoc-sqr/smoc_synth_std_includes.hpp
@@ -9,6 +9,10 @@
 
 // Maximum (and default) number of Src iterations. Lower default number via
 //  command line parameter.
+#if defined(SYSTEMC_VERSION) || defined(SQR_LOGGING)
 const int NUM_MAX_ITERATIONS = 1000000;
+#else
+const int NUM_MAX_ITERATIONS = 250000000;
+#endif //defined(SYSTEMC_VERSION) || defined(SQR_LOGGING)
 
 #endif // INCLUDED_SMOC_SYNTH_STD_INCLUDES_HPP
