ad65f5b96df47653dcca7928f896e1f0445007ae
Joachim Falk <joachim.falk@fau.de>
2017-08-01 14:20:16 +0200

    Added forgotten smoc_register.hpp include to smoc_moc.hpp header
    Conflicts:
    	src/systemoc/headers/systemoc/smoc_moc.hpp

--- src/systemoc/headers/systemoc/smoc_moc.hpp
+++ src/systemoc/headers/systemoc/smoc_moc.hpp
@@ -47,6 +47,7 @@
 #include <systemoc/smoc_fifo.hpp>
 #include <systemoc/smoc_multireader_fifo.hpp>
 #include <systemoc/smoc_multiplex_fifo.hpp>
+#include <systemoc/smoc_register.hpp>
 #include <systemoc/smoc_actor.hpp>
 #include <systemoc/smoc_periodic_actor.hpp>
 #include <systemoc/smoc_graph.hpp>
