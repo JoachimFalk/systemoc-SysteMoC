e672973019c22aba0a6dab9cf021d4935c08b4a8
Joachim Falk <joachim.falk@fau.de>
2017-08-04 11:21:38 +0200

    Fixed test cases for NDEBUG case.

--- src/test-cluster-example/cpp/cluster_ex.cpp
+++ src/test-cluster-example/cpp/cluster_ex.cpp
@@ -7,25 +7,18 @@
 #include "DynSwitch.hpp"
 #include "StaticCluster.hpp"
 
-#ifndef KASCPAR_PARSING    
-using namespace std;
-#endif
-
 template<int upsample_factor>
-class m_top
-  : public smoc_graph
-{
+class m_top: public smoc_graph {
 private:
   StaticCluster<upsample_factor> static_cluster;
   DynSwitch dyn_switch;
 
 public:
   m_top<upsample_factor>(sc_core::sc_module_name name)
-    : smoc_graph(name),
-      static_cluster("static_cluster",5,10),
-      dyn_switch("dyn_switch",1000)
+    : smoc_graph(name)
+    , static_cluster("static_cluster", 5, 10)
+    , dyn_switch("dyn_switch", 1000)
   {
-    
     connectNodePorts<2>(dyn_switch.out1,static_cluster.in1);
     connectNodePorts<1>(dyn_switch.out2,static_cluster.in2);
 
@@ -35,12 +28,8 @@ public:
 
 };
 
-
-
 int sc_main (int argc, char **argv) {
-#ifndef KASCPAR_PARSING    
   smoc_top_moc<m_top<10> > top("top");
-#endif
   sc_core::sc_start();
   return 0;
 }
--- src/test-getValueAt/cpp/design-getValueAt.cpp
+++ src/test-getValueAt/cpp/design-getValueAt.cpp
@@ -4,68 +4,60 @@
 #include <iostream>
 
 #include <systemoc/smoc_moc.hpp>
-#include <systemoc/smoc_port.hpp>
-#include <systemoc/smoc_fifo.hpp>
-#include <systemoc/smoc_actor.hpp>
-#include <systemoc/smoc_graph.hpp>
-
-using namespace std;
 
 class m_source: public smoc_actor {
-  public:
-    smoc_port_out<int> out;
-  private:
-    size_t i;
-    
-    void process() {
-#ifndef NDEBUG
-      cout << name() << " generating " << i << std::endl;
-#endif
-      out[0] = i++;
-    }
+public:
+  smoc_port_out<int> out;
+private:
+  size_t i;
 
-    smoc_firing_state start;
-  public:
-    m_source( sc_core::sc_module_name name, size_t iter)
-      :smoc_actor( name, start ), i(0) {
-      start =  out(1) >> (SMOC_VAR(i) < iter) >> SMOC_CALL(m_source::process) >> start;
-    }
+  void process() {
+    std::cout << name() << " generating " << i << std::endl;
+    out[0] = i++;
+  }
+
+  smoc_firing_state start;
+public:
+  m_source( sc_core::sc_module_name name, size_t iter)
+    : smoc_actor( name, start ), i(0) {
+    start =
+        (out(1) && SMOC_VAR(i) < iter) >>
+        SMOC_CALL(m_source::process) >> start
+      ;
+  }
 };
 
 class m_sink: public smoc_actor {
-  public:
-    smoc_port_in<int> in;
-  private:
-    void process() {
-#ifndef NDEBUG
-      cout << name() << " receiving " << in[0] << std::endl;
-#endif
-    }
-    
-    smoc_firing_state start;
-  public:
-    m_sink( sc_core::sc_module_name name )
-      :smoc_actor( name, start ) {
-      start = (in(1) && in.getValueAt(0) == 0) 
-				>> SMOC_CALL(m_sink::process) 
-				>> start
-				| in(1) 
-				>> SMOC_CALL(m_sink::process) 
-				>> start;
-    }
+public:
+  smoc_port_in<int> in;
+private:
+  void process() {
+    std::cout << name() << " receiving " << in[0] << std::endl;
+  }
+
+  smoc_firing_state start;
+public:
+  m_sink( sc_core::sc_module_name name )
+    :smoc_actor( name, start ) {
+    start =
+        (in(1) && in.getValueAt(0) == 0) >>
+        SMOC_CALL(m_sink::process) >> start
+      |
+        in(1) >>
+        SMOC_CALL(m_sink::process) >> start
+      ;
+  }
 };
 
-class m_top
-: public smoc_graph {
-  public:
-    m_top( sc_core::sc_module_name name, size_t iter)
-      : smoc_graph(name) {
-      m_source      &src1 = registerNode(new m_source("src1", iter));
-      m_sink        &sink = registerNode(new m_sink("sink"));
-#ifndef KASCPAR_PARSING      
-      connectNodePorts( src1.out, sink.in, smoc_fifo<int>(2) );
-#endif
-    }
+class m_top: public smoc_graph {
+public:
+  m_top( sc_core::sc_module_name name, size_t iter)
+    : smoc_graph(name)
+  {
+    m_source &src1 = registerNode(new m_source("src1", iter));
+    m_sink   &sink = registerNode(new m_sink("sink"));
+    connectNodePorts(src1.out, sink.in, smoc_fifo<int>(2));
+  }
 };
 
 int sc_main (int argc, char **argv) {
