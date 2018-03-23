a8de7ce7e7bdf0e5f6aacd3702f4215285525156
Joachim Falk <joachim.falk@fau.de>
2018-03-23 12:08:57 +0100

    Got rid of more CALL and VAR macro uses. Renamed some tests.

--- src/test-fsm-action-parameters/cpp/design-fsm-action-parameters.cpp
+++ src/test-fsm-action-parameters/cpp/design-fsm-action-parameters.cpp
@@ -1,3 +1,4 @@
+//  -*- tab-width:8; intent-tabs-mode:nil;  c-basic-offset:2; -*-
 // vim: set sw=2 ts=8:
 /*
  * Copyright (c) 2004-2006 Hardware-Software-CoDesign, University of
@@ -33,98 +34,81 @@
  * ENHANCEMENTS, OR MODIFICATIONS.
  */
 
-#include <cstdlib>
-#include <iostream>
-
 #include <systemoc/smoc_moc.hpp>
 #include <systemoc/smoc_port.hpp>
 #include <systemoc/smoc_fifo.hpp>
 #include <systemoc/smoc_actor.hpp>
 #include <systemoc/smoc_graph.hpp>
 
-template <typename T>
-class m_h_src: public smoc_actor {
+#include <systemc.h>
+#include <tlm.h>
+
+#include <iostream>
+
+class Src : public smoc_actor {
 public:
-  smoc_port_out<T> out;
-private:
-  T       i;
-  size_t  iter;
-  
+  smoc_port_out<void> out;
+
+  Src(sc_core::sc_module_name name, size_t iter)
+    : smoc_actor(name, run), iter(iter)
+  {
+    run =
+        (out(1) && (SMOC_VAR(this->iter) != 0U)) >>
+        SMOC_CALL(Src::src) >> run
+     ;
+  }
+
   void src() {
-    std::cout
-      << name() << ": generate token with value " << i << ", " << i+1 << std::endl;
-    out[0] = i++;
-    out[1] = i++;
+    std::cout << "Src::src()" << std::endl;
     --iter;
   }
-  smoc_firing_state start;
-public:
-  m_h_src(sc_core::sc_module_name name, size_t _iter)
-    : smoc_actor(name, start),
-      i(1), iter(_iter) {
-    start =
-         (out(2) && !(SMOC_VAR(iter) == 0U))
-      >> SMOC_CALL(m_h_src::src)       >> start;
-  }
-};
 
-
-template <typename T>
-class m_h_sink: public smoc_actor {
-public:
-  smoc_port_in<T> in;
 private:
-//  int i;
-  
-  void sink(void) {
-    std::cout
-      << name() << ": received token with value " << in[0] << ", " << in[1] << ", and " << in[2] << std::endl;
-  }
-  smoc_firing_state start;
-public:
-  m_h_sink(sc_core::sc_module_name name)
-    : smoc_actor(name, start) {
-    start = in(3) >> SMOC_CALL(m_h_sink::sink) >> start;
-  }
+  size_t iter;
+
+  smoc_firing_state run;
 };
 
-//class m_h_dummy: public smoc_actor {
-//private:
-//  void dummy() {
-//    std::cout
-//      << name() << ": dummy computational payload" << std::endl;
-//  }
-//  smoc_firing_state start;
-//public:
-//  m_h_dummy(sc_core::sc_module_name name)
-//    : smoc_actor(name, start) {
-//    start = SMOC_CALL(m_h_dummy::dummy) >> start;
-//  }
-//};
-
-
-class m_h_top: public smoc_graph {
-protected:
-  m_h_src<int>     src;
-  m_h_sink<int>    snk;
-//  m_h_dummy        dummy;
+class Snk : public smoc_actor {
 public:
-  m_h_top(sc_core::sc_module_name name, size_t iter)
+  smoc_port_in<void> in;
+
+  Snk(sc_core::sc_module_name name) :
+    smoc_actor(name, run)
+  {
+    run = in(1) >> SMOC_CALL(Snk::snk) >> run;
+  }
+
+  void snk() {
+    std::cout << "Snk::snk()" << std::endl;
+  }
+
+private:
+  smoc_firing_state run;
+};
+
+class Graph : public smoc_graph {
+public:
+  Graph(sc_core::sc_module_name name, size_t iter)
     : smoc_graph(name),
-      src("src", iter), snk("snk") {//, dummy("dummy")
-    smoc_fifo<int> f(4);
-    f.connect(src.out).connect(snk.in);
-//  connectNodePorts(src.out, snk.in, f);
+      src("src", iter),
+      snk("snk")
+  {
+    connectNodePorts(src.out,snk.in);
   }
+private:
+  Src src;
+  Snk snk;
 };
 
-int sc_main (int argc, char **argv) {
+int sc_main (int argc, char **argv) { 
   size_t iter = static_cast<size_t>(-1);
   
   if (argc >= 2)
     iter = atol(argv[1]);
-  
-  smoc_top_moc<m_h_top> top("top", iter);
+
+  Graph g("g", iter);
+  smoc_scheduler_top top(g);
   sc_core::sc_start();
   return 0;
 }
