9b9c790382f4428b11241bc03a0279c50482ef62
Joachim Falk <joachim.falk@fau.de>
2018-03-27 11:36:41 +0200

    Continue to refactor FSM (user facing side).

--- src/systemoc/headers/smoc/smoc_firing_rule.hpp
+++ src/systemoc/headers/smoc/smoc_firing_rule.hpp
@@ -2,29 +2,29 @@
 // vim: set sw=2 ts=8 et:
 /*
  * Copyright (c) 2018 Hardware-Software-CoDesign, University of Erlangen-Nuremberg.
- *
+ * 
  *   This library is free software; you can redistribute it and/or modify it under
  *   the terms of the GNU Lesser General Public License as published by the Free
  *   Software Foundation; either version 2 of the License, or (at your option) any
  *   later version.
- *
+ * 
  *   This library is distributed in the hope that it will be useful, but WITHOUT
  *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  *   details.
- *
+ * 
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this library; if not, write to the Free Software Foundation, Inc.,
  *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
- * --- This software and any associated documentation is provided "as is"
- *
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
  * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
  * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
  * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
  * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
  * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
  * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
@@ -33,49 +33,59 @@
  * ENHANCEMENTS, OR MODIFICATIONS.
  */
 
-#ifndef _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
-#define _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
+#ifndef _INCLUDED_SMOC_SMOC_FIRING_RULE_HPP
+#define _INCLUDED_SMOC_SMOC_FIRING_RULE_HPP
 
-#include <CoSupport/SmartPtr/intrusive_refcount_ptr.hpp>
-#include <CoSupport/DataTypes/Facade.hpp>
+#include "../systemoc/detail/smoc_func_call.hpp"
+#include "smoc_expr.hpp"
 
 namespace smoc {
 
-  class smoc_transition_list;
+typedef Expr::Ex<bool>::type Guard;
+
+class smoc_firing_rule {
+public:
+  typedef smoc_firing_rule this_type;
+
+private:
+  /// @brief guard (AST assembled from smoc_expr.hpp nodes)
+  Guard       guard;
+  /// @brief Action
+  smoc_action action;
+public:
+  /// @brief Constructor
+  explicit smoc_firing_rule(Guard const &g)
+    : guard(g) {}
+
+  /// @brief Constructor
+  explicit smoc_firing_rule(Guard const &g, smoc_action const &a)
+    : guard(g), action(a) {}
+
+  /// @brief Returns the guard
+  Guard const &getGuard() const
+    { return guard; }
+
+  /// @brief Returns the action
+  smoc_action const &getAction() const
+    { return action; }
+};
+
+inline
+smoc_firing_rule operator >> (
+    smoc_firing_rule const &tp,
+    smoc_action      const &a)
+  { return smoc_firing_rule(tp.getGuard(), merge(tp.getAction(), a)); }
+
+namespace Expr {
+
+  template <class E>
+  smoc_firing_rule operator >> (
+      Expr::D<E>  const &g,
+      smoc_action const &a)
+    { return smoc_firing_rule(Guard(g), a); }
+
+} // namespace Expr
 
 } // namespace smoc
 
-namespace smoc { namespace Detail {
-
-  class FiringStateBaseImpl;
-  DECL_INTRUSIVE_REFCOUNT_PTR(FiringStateBaseImpl, PFiringStateBaseImpl);
-
-  class FiringStateBase
-  : public CoSupport::DataTypes::FacadeFoundation<
-      FiringStateBase,
-      FiringStateBaseImpl
-    >
-  {
-  public:
-    typedef FiringStateBase this_type;
-
-    friend class FiringStateBaseImpl;
-  protected:
-    explicit FiringStateBase(_StorageType const &x): FFType(x) {}
-    FiringStateBase(SmartPtr const &p);
-  public:
-    ImplType *getImpl() const;
-
-    /// @brief Add transitions to state
-    void addTransition(smoc_transition_list const &tl);
-
-    /// @brief Clear all transitions
-    void clearTransition();
-
-    this_type& operator=(smoc_transition_list const &tl);
-    this_type& operator|=(smoc_transition_list const &tl);
-  };
-
-} } // namespace smoc::Detail
-
-#endif // _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
+#endif // _INCLUDED_SMOC_SMOC_FIRING_RULE_HPP
--- src/systemoc/headers/systemoc/smoc_transition.hpp
+++ src/systemoc/headers/systemoc/smoc_transition.hpp
@@ -2,29 +2,29 @@
 // vim: set sw=2 ts=8 et:
 /*
  * Copyright (c) 2018 Hardware-Software-CoDesign, University of Erlangen-Nuremberg.
- *
+ * 
  *   This library is free software; you can redistribute it and/or modify it under
  *   the terms of the GNU Lesser General Public License as published by the Free
  *   Software Foundation; either version 2 of the License, or (at your option) any
  *   later version.
- *
+ * 
  *   This library is distributed in the hope that it will be useful, but WITHOUT
  *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  *   FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  *   details.
- *
+ * 
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this library; if not, write to the Free Software Foundation, Inc.,
  *   59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
- * --- This software and any associated documentation is provided "as is"
- *
+ * 
+ * --- This software and any associated documentation is provided "as is" 
+ * 
  * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
  * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
  * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
  * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
  * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
  * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
@@ -33,49 +33,12 @@
  * ENHANCEMENTS, OR MODIFICATIONS.
  */
 
-#ifndef _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
-#define _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
+#ifndef _INCLUDED_SYSTEMOC_SMOC_TRANSITION_HPP
+#define _INCLUDED_SYSTEMOC_SMOC_TRANSITION_HPP
 
-#include <CoSupport/SmartPtr/intrusive_refcount_ptr.hpp>
-#include <CoSupport/DataTypes/Facade.hpp>
+#include "../smoc/smoc_transition.hpp"
 
-namespace smoc {
+using smoc::smoc_transition;
+using smoc::smoc_transition_list;
 
-  class smoc_transition_list;
-
-} // namespace smoc
-
-namespace smoc { namespace Detail {
-
-  class FiringStateBaseImpl;
-  DECL_INTRUSIVE_REFCOUNT_PTR(FiringStateBaseImpl, PFiringStateBaseImpl);
-
-  class FiringStateBase
-  : public CoSupport::DataTypes::FacadeFoundation<
-      FiringStateBase,
-      FiringStateBaseImpl
-    >
-  {
-  public:
-    typedef FiringStateBase this_type;
-
-    friend class FiringStateBaseImpl;
-  protected:
-    explicit FiringStateBase(_StorageType const &x): FFType(x) {}
-    FiringStateBase(SmartPtr const &p);
-  public:
-    ImplType *getImpl() const;
-
-    /// @brief Add transitions to state
-    void addTransition(smoc_transition_list const &tl);
-
-    /// @brief Clear all transitions
-    void clearTransition();
-
-    this_type& operator=(smoc_transition_list const &tl);
-    this_type& operator|=(smoc_transition_list const &tl);
-  };
-
-} } // namespace smoc::Detail
-
-#endif // _INCLUDED_SMOC_DETAIL_FIRINGSTATEBASE_HPP
+#endif // _INCLUDED_SYSTEMOC_SMOC_TRANSITION_HPP
