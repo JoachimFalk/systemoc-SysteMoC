07f6e3b264d399dc8d0e4f9c01bd3d07269d133c
Joachim Falk <joachim.falk@fau.de>
2018-06-15 14:46:56 +0200

    Added checkFiringRule and executeFiringRule SchedulerInterface methods to replace Director::check and Director::compute.

--- src/systemoc/headers/smoc/SimulatorAPI/SchedulerInterface.hpp
+++ src/systemoc/headers/smoc/SimulatorAPI/SchedulerInterface.hpp
@@ -43,11 +43,17 @@ namespace smoc { namespace SimulatorAPI {
 
   class SchedulerInterface {
   public:
+    // This must be implemented by the scheduler and will be called by
+    // SysteMoC for each firing rule of a SysteMoC actor (task).
+    virtual void registerFiringRule(TaskInterface *task, FiringRuleInterface *fr) = 0;
+
     // This must be implemented by the scheduler and will be called by the
     // SysteMoC task if task->getUseActivationCallback() returns true.
     virtual void notifyActivation(TaskInterface *task, bool activation) = 0;
 
-    virtual void registerFiringRule(TaskInterface *task, FiringRuleInterface *fr) = 0;
+    virtual void checkFiringRule(FiringRuleInterface *fr) = 0;
+
+    virtual void executeFiringRule(FiringRuleInterface *fr) = 0;
 
     virtual ~SchedulerInterface() {}
   };
