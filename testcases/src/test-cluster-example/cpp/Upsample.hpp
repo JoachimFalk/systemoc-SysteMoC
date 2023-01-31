73af6e85a0a7b41292014bb18a11b107392c76c5
Joachim Falk <joachim.falk@fau.de>
2023-01-31 12:35:38 +0100

    Added copyright to all test case source files.

--- testcases/src/test-cluster-example/cpp/Upsample.hpp
+++ testcases/src/test-cluster-example/cpp/Upsample.hpp
@@ -1,5 +1,26 @@
-// -*- tab-width:8; intent-tabs-mode:nil; c-basic-offset:2; -*-
-// vim: set sw=2 ts=8:
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:2; -*-
+// vim: set sw=2 ts=8 et:
+/*
+ * Copyright (c)
+ *   2010 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2016 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2017 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2019 FAU -- Joachim Falk <joachim.falk@fau.de>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA.
+ */
 
 #ifndef _INCLUDED_UPSAMPLE
 #define _INCLUDED_UPSAMPLE
--- testcases/src/test-fsm-hierarchy-xor-diss-falk/cpp/design-fsm-hierarchy-xor.cpp
+++ testcases/src/test-fsm-hierarchy-xor-diss-falk/cpp/design-fsm-hierarchy-xor.cpp
@@ -1,36 +1,28 @@
-// vim: set sw=2 ts=8:
+// -*- tab-width:8; indent-tabs-mode:nil; c-basic-offset:2; -*-
+// vim: set sw=2 ts=8 et:
 /*
- * Copyright (c) 2004-2006 Hardware-Software-CoDesign, University of
- * Erlangen-Nuremberg. All rights reserved.
+ * Copyright (c)
+ *   2010 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2011 FAU -- Martin Streubuehr <martin.streubuehr@fau.de>
+ *   2012 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2016 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2017 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2018 FAU -- Joachim Falk <joachim.falk@fau.de>
+ *   2019 FAU -- Joachim Falk <joachim.falk@fau.de>
  * 
- *   This program is free software; you can redistribute it and/or modify it under
- *   the terms of the GNU General Public License as published by the Free Software
- *   Foundation; either version 2 of the License, or (at your option) any later
- *   version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  * 
- *   This program is distributed in the hope that it will be useful, but WITHOUT
- *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- *   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
- *   details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  * 
- *   You should have received a copy of the GNU General Public License along with
- *   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- *   Place, Suite 330, Boston, MA 02111-1307 USA.
- * 
- * --- This software and any associated documentation is provided "as is" 
- * 
- * IN NO EVENT SHALL HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG
- * BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR
- * CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
- * DOCUMENTATION, EVEN IF HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN
- * NUREMBERG HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- * HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF ERLANGEN NUREMBERG, SPECIFICALLY
- * DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
- * HEREUNDER IS ON AN "AS IS" BASIS, AND HARDWARE-SOFTWARE-CODESIGN, UNIVERSITY OF
- * ERLANGEN NUREMBERG HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- * ENHANCEMENTS, OR MODIFICATIONS.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #include <iostream>
